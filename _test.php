43e702b1165881ef56195c51634f005a98d7a179
kim <markkimsacluti10101996@gmail.com>
2020-02-26 12:49:34 +0800

    rename file

--- _test.php
+++ _test.php
@@ -15,43 +15,22 @@ $mydb = new db(); // create a new object, class db()
 
 <body>
 <div class="row">
-<div class="col-md-12">
-    <div class="box">
-      <div class="panel panel-default">
-        <div class="box-body"> 
-          <div class="">
-            
-            <h1 align="">Technical Assistance Request Form</h1>
-           <br>
-            
-
-            <li class="btn btn-success"><a href="CreatePR1.php" style="color:white;text-decoration: none;">Create</a></li>
-
-            <br>
-            <br>
-
-             <!-- &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" style="height: 35px; width: 400px" id="myInput" onkeyup="myFunction()" placeholder="Search Here" > -->
-
-            <br>
-            <br>
-   
-            <!-- <h3 align="center"><b>Inspection Acceptance Report</b></h3> -->
-
-         
-            
+    <div class="col-md-12">
+        <div class="box">
+            <div class="panel panel-default">
+                <div class="box-body">      
+                    <div>
+                        <h1>Technical Assistance Request Form</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-</div>
-</div>
+ </body>
+</html>
 
-</body>
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#example1').DataTable();
-
-    } );
-</script>
 
 <!-- jQuery 3 -->
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
@@ -83,92 +62,6 @@ $mydb = new db(); // create a new object, class db()
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
 <!-- Page script -->
-<script>
-  $(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-    //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-    //Money Euro
-    $('[data-mask]').inputmask()
-
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, locale: { format: 'MM/DD/YYYY hh:mm A' }})
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
-
-    //Date picker,
-    $('#datepicker1').datepicker({
-      autoclose: true
-    })
-
-    $('#datepicker2').datepicker({
-      autoclose: true
-    })
-    $('#datepicker3').datepicker({
-      autoclose: true
-    })
-    $('#datepicker4').datepicker({
-      autoclose: true
-    })
-
-    //iCheck for checkbox and radio inputs
-    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-      checkboxClass: 'icheckbox_minimal-blue',
-      radioClass   : 'iradio_minimal-blue'
-    })
-    //Red color scheme for iCheck
-    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-      checkboxClass: 'icheckbox_minimal-red',
-      radioClass   : 'iradio_minimal-red'
-    })
-    //Flat red color scheme for iCheck
-    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-      checkboxClass: 'icheckbox_flat-green',
-      radioClass   : 'iradio_flat-green'
-    })
-
-    //Colorpicker
-    $('.my-colorpicker1').colorpicker()
-    //color picker with addon
-    $('.my-colorpicker2').colorpicker()
-
-    //Timepicker
-    $('.timepicker').timepicker({
-      showInputs: false
-    })
-  })
-</script>
 
 
-</div>
-</div>
 
-
-<div class="panel-footer"></div>
-</div>
-</div>
-
-</body>
-</html>
