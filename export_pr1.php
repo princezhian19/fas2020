92c5b80f85c4fc510cb53195b120086ddfee0364
Zhian Ferrer <zhianferrer019@gmail.com>
2020-02-27 09:44:15 +0800

    pr export pass

--- export_pr1.php
+++ export_pr1.php
@@ -178,7 +178,12 @@ if ($unit == "21") {
     $objPHPExcel->setActiveSheetIndex()->setCellValue('F'.$row,$total);
 
 
+    $objPHPExcel->getActiveSheet()->getProtection()->setSheet(true);
+    $objPHPExcel->getActiveSheet()->getProtection()->setSort(true);
+    $objPHPExcel->getActiveSheet()->getProtection()->setInsertRows(true);
+    $objPHPExcel->getActiveSheet()->getProtection()->setFormatCells(true);
 
+    $objPHPExcel->getActiveSheet()->getProtection()->setPassword('fas2020');
        // $objPHPExcel->setActiveSheetIndex()->setCellValue('G'.$row,$excelrow['abc']);
     // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($stylebottom);
     // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleTop);
