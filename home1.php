4c5fe505b5f05527fb418148ef5b2b94bd5c2df3
charles <charlesodi1324@gmail.com>
2020-03-28 23:14:36 +0800

    habol

--- home1.php
+++ home1.php
@@ -13,18 +13,17 @@ header('location:index.php');
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="shortcut icon" type="image/png" href="dilg.png">
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
   <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-<<<<<<< HEAD
+
   <link rel="stylesheet" href="_includes/fontawesome.css">
-=======
   
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
->>>>>>> d86344815dffc1c85eb531575689c5ef20bce9f9
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
