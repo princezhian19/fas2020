95f21f28c92f367066bec74f004f878a80f20c08
kim <markkimsacluti10101996@gmail.com>
2020-03-30 13:45:26 +0800

    plugins datatable and fixed session

--- DATATABLE/index.php
+++ DATATABLE/index.php
@@ -1,13 +1,15 @@
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="css/jquery.dataTables.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
     <!-- datatable lib -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="js/jquery-1.12.4.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+
+
+    
 <table id="example" class="display" style="width:100%">
         <thead>
             <tr>
@@ -29,36 +31,40 @@
             </tr>
         </tfoot>
     </table>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script> -->
+<button id = "sweet-14">a</button>
     <script>
            
 $(document).ready(function() {
-    
 
-    $('#example').DataTable( {
+
+    
+    var table = $('#example').DataTable( {
         "processing": true,
         "serverSide": false,
         "ajax": "server_processing.php",
-
-
         columnDefs:   
-                [
-                          {"className": "dt-center", "targets": "_all"},
+            [
+                {"className": "dt-center", "targets": "_all"},  
                     {
-                    targets: [-1], render: function (a, b, data, d) {
-                      
-                        if(data[0] == 2563 || data[0] == 2577 )
-            {
-              return "<button class = 'btn btn-primary'>Activate</button>";
-                
-            }else{
-              return "<button class = 'btn btn-danger'>Inactive</button>";
-
+                        targets: [-1], render: function (a, b, data, d) 
+                        {
+                            var a = data[0];
+                            if(data[0] == 2563 || data[0] == 2577 )
+                            {
+                                return '<a class="btn btn-info btn-sm" id="sweet-14">' + 'Edit' + '</a>';
+                            }else{
+                            return "<button class = 'btn btn-danger'>Inactive</button>";
+                            }
+                            
             }
-                    }
-                }],
+            }],
+    } );
+    $('#example tbody').on( 'click', '#sweet-14', function () {
+    var data = table.row( $(this).parents('tr') ).data();
+    alert( data[0] +"'s salary is: "+ data[ 5 ] );
     } );
 } );
+
     </script>
