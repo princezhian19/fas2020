7942f36995d6c944a24d9e213aef4289c6d4a576
kim <markkimsacluti10101996@gmail.com>
2020-04-01 13:26:56 +0800

    commit export issue

--- _fmlReport.php
+++ _fmlReport.php
@@ -1,17 +1,7 @@
 
 <?php
-    date_default_timezone_set('Asia/Manila');
-
-    include 'connection.php';
-    // include 'connection.php';
-    // $username = $_SESSION['username'];
-    // $query = "SELECT * FROM tbltechnical_assistance WHERE `STATUS_REQUEST` = 'For action' order by `REQ_DATE` DESC, `REQ_TIME` LIMIT 4 ";
-    // $result = mysqli_query($conn, $query);
-    // if ($result->num_rows > 0) {
-
-    // echo '<ul class="list-group list-group-flush">';
-
-    // whi
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+$conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
 require_once 'library/PHPExcel/Classes/PHPExcel/IOFactory.php';
 $objPHPExcel = PHPExcel_IOFactory::load("library/fml.xlsx");
 $styleTop = array(
@@ -64,99 +54,31 @@ if ($result->num_rows > 0) {
 
     if(strlen($excelrow['REQ_BY']) >= 10)
     {
-    $objPHPExcel->getActiveSheet(0)->getRowDimension($row)->setRowHeight(53);
-    $objPHPExcel->getActiveSheet(0)->getStyle('E'.$row)->getAlignment()->setWrapText(true);
+      $objPHPExcel->getActiveSheet(0)->getRowDimension($row)->setRowHeight(53);
+      $objPHPExcel->getActiveSheet(0)->getStyle('E'.$row)->getAlignment()->setWrapText(true);
 
     }else{
-    $objPHPExcel->getActiveSheet(2)->getRowDimension($row)->setRowHeight(14.40);
-    $objPHPExcel->getActiveSheet(2)->getStyle('E'.$row)->getAlignment()->setWrapText(true);
+      $objPHPExcel->getActiveSheet(2)->getRowDimension($row)->setRowHeight(14.40);
+      $objPHPExcel->getActiveSheet(2)->getStyle('E'.$row)->getAlignment()->setWrapText(true);
     }
 
     $startTime = date('Y-m-d',strtotime($excelrow['START_DATE']));
     $endTime = date('Y-m-d',strtotime($excelrow['COMPLETED_DATE']));
-    
-
-
+  
     $date1 = strtotime($startTime."".$excelrow['START_TIME']);  
     $date2 = strtotime($endTime."".$excelrow['COMPLETED_TIME']);  
       
-    // Formulate the Difference between two dates 
     $diff = abs($date2 - $date1);  
-      
-      
-    // To get the year divide the resultant date into 
-    // total seconds in a year (365*60*60*24) 
     $years = floor($diff / (365*60*60*24));  
+    $months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));  
+    $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24)); 
       
-      
-    // To get the month, subtract it with years and 
-    // divide the resultant date into 
-    // total seconds in a month (30*60*60*24) 
-    $months = floor(($diff - $years * 365*60*60*24) 
-                                / (30*60*60*24));  
-    
-    
-    // To get the day, subtract it with years and  
-    // months and divide the resultant date into 
-    // total seconds in a days (60*60*24) 
-    $days = floor(($diff - $years * 365*60*60*24 -  
-                $months*30*60*60*24)/ (60*60*24)); 
-      
-      
-    // To get the hour, subtract it with years,  
-    // months & seconds and divide the resultant 
-    // date into total seconds in a hours (60*60) 
-    $hours = floor(($diff - $years * 365*60*60*24  
-          - $months*30*60*60*24 - $days*60*60*24) 
-                                      / (60*60));  
-      
-      
-    // To get the minutes, subtract it with years, 
-    // months, seconds and hours and divide the  
-    // resultant date into total seconds i.e. 60 
-    $minutes = floor(($diff - $years * 365*60*60*24  
-            - $months*30*60*60*24 - $days*60*60*24  
-                              - $hours*60*60)/ 60);  
-      
-      
-    // To get the minutes, subtract it with years, 
-    // months, seconds, hours and minutes  
-    $seconds = floor(($diff - $years * 365*60*60*24  
-            - $months*30*60*60*24 - $days*60*60*24 
-                    - $hours*60*60 - $minutes*60));  
-      
-    // Print the result 
+     
+    $hours = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24) / (60*60));  
+    $minutes = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60)/ 60);  
+    $seconds = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60 - $minutes*60));  
     $calc_mins = sprintf("%d hours and %d minutes",$hours, $minutes);
-        // echo $excelrow['START_TIME'];
-
         
-        
-          // $calc_mins = '';
-          // $to_time_format = date('g:i',strtotime($excelrow['START_DATE'].''.$excelrow['START_TIME']));
-          // $from_time_format = date('g:i',strtotime($excelrow['COMPLETED_DATE'].''.$excelrow['COMPLETED_TIME']));
-          // $to_time = strtotime($excelrow['START_DATE'].''.$to_time_format);
-          // $from_time = strtotime($excelrow['COMPLETED_DATE'].''.$from_time_format);
-          // $calc_mins = round(abs($to_time - $from_time) / 60,2);
-          
-          // if($calc_mins < 60)
-          // {
-          //   $calc_mins = round(abs($to_time - $from_time) / 60,2).' minutes';
-          //   echo  $calc_mins.'<br>';
-          // }else{
-            
-          //   $date1 = new DateTime($excelrow['START_DATE'].''.$excelrow['START_TIME']);
-          //   $date2 = new DateTime($excelrow['COMPLETED_DATE'].''.$excelrow['COMPLETED_TIME']);
-            
-          //   $diff = $date2->diff($date1);
-            
-          //   $hours = $diff->h;
-          //   $hours = $hours + ($diff->days*24);
-          //   $minutes = ($calc_mins % 60);
-            
-          //   echo $hours.'hours and '.$minutes.'<br>';
-          // }
-    
-      
       
       
 
@@ -196,8 +118,6 @@ if ($result->num_rows > 0) {
 
 
 
-
-$objPHPExcel->setActiveSheetIndex(0);
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $objWriter->save(str_replace('.php', '.xlsx', __FILE__));
 header('location:_fmlReport.xlsx');
