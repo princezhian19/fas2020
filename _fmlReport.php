96e744e0e32428cb9f058a2843159766614a6b9f
kim <markkimsacluti10101996@gmail.com>
2020-04-01 16:45:57 +0800

    last try

--- _fmlReport.php
+++ _fmlReport.php
@@ -27,350 +27,113 @@ $stylebottom = array(
   ),
 );
 
-$styleHeader = array('font'  => array('bold'  => true, 'size'  => 11, 'name'  => 'Calibri'),'alignment' => array('horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER));
 
- $styleLabel = array('font'  => array('size'  => 11, 'name'  => 'Calibri'),'alignment' => array('horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_LEFT));
 
 $conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
-$id = $_GET['id'];
+$month = $_GET['month'];
+$year = $_GET['year'];
 
-$sql = mysqli_query($conn, "SELECT * FROM pr WHERE id = '$id' ");
-$row = mysqli_fetch_array($sql);
-$pr_no = $row['pr_no'];
-$pmo = $row['pmo'];
-$purpose = $row['purpose'];
-$pr_date = $row['pr_date'];
+$sql_q10 = mysqli_query($conn, "SELECT MONTHNAME(`REQ_DATE`) AS 'month', YEAR(`REQ_DATE`) AS 'year', `ID`, `CONTROL_NO`, `REQ_DATE`, `REQ_TIME`, `REQ_BY`, `OFFICE`, `POSITION`, `CONTACT_NO`, `EMAIL_ADD`, `EQUIPMENT_TYPE`, `BRAND_MODEL`, `PROPERTY_NO`, `SERIAL_NO`, `IP_ADDRESS`, `MAC_ADDRESS`, `TYPE_REQ`, `TYPE_REQ_DESC`, `ISSUE_PROBLEM`, `START_DATE`, `START_TIME`, `STATUS_DESC`, `COMPLETED_DATE`, `COMPLETED_TIME`, `ASSIST_BY`, `PERSON_ASSISTED`, `TIMELINESS`, `QUALITY`, `STATUS` FROM `tbltechnical_assistance` WHERE MONTH(REQ_DATE) = $month and YEAR(REQ_DATE) = $year ");
+    if (mysqli_num_rows($sql_q10)>0) {
+    $row = 15;
+    $no = 1;
+    while($excelrow= mysqli_fetch_assoc($sql_q10) ) 
+    {
 
-$d1 = date('F d, Y', strtotime($pr_date));
+      if(strlen($excelrow['REQ_BY']) >= 10)
+      {
+      $objPHPExcel->getActiveSheet(0)->getRowDimension($row)->setRowHeight(53);
+      $objPHPExcel->getActiveSheet(0)->getStyle('E'.$row)->getAlignment()->setWrapText(true);
 
+      }else{
+      $objPHPExcel->getActiveSheet(2)->getRowDimension($row)->setRowHeight(14.40);
+      $objPHPExcel->getActiveSheet(2)->getStyle('E'.$row)->getAlignment()->setWrapText(true);
+      }
 
-$objPHPExcel->setActiveSheetIndex()->setCellValue('B7',$pmo);
-$objPHPExcel->setActiveSheetIndex()->setCellValue('C7','PR No.:  '.$pr_no);
-if($pr_date == '0000-00-00'){
-$objPHPExcel->setActiveSheetIndex()->setCellValue('F7',"");  
-
-}
-else{
-  
-
-$objPHPExcel->setActiveSheetIndex()->setCellValue('F7',$d1);  
-}
-
-$totalcount = mysqli_query($conn, "SELECT sum(pr.qty) as first ,sum(pr.abc) as second FROM pr_items pr left join app a on a.id = pr.items WHERE pr.pr_no = '$pr_no' "); 
+      $startTime = date('Y-m-d',strtotime($excelrow['START_DATE']));
+      $endTime = date('Y-m-d',strtotime($excelrow['COMPLETED_DATE']));
+      
 
 
-  
-  //   $ans = $excelrow1['first']*$excelrow1['second'];
-  //   echo $ans;
-  //   exit();
-  // while($excelrow1 = mysqli_fetch_assoc($totalcount) ){
-  // }
+      $date1 = strtotime($startTime."".$excelrow['START_TIME']);  
+      $date2 = strtotime($endTime."".$excelrow['COMPLETED_TIME']);  
+    
+      // Formulate the Difference between two dates 
+      $diff = abs($date2 - $date1);  
+    
+    
+      // To get the year divide the resultant date into 
+      // total seconds in a year (365*60*60*24) 
+      $years = floor($diff / (365*60*60*24));  
+        
+        
+      // To get the month, subtract it with years and 
+      // divide the resultant date into 
+      // total seconds in a month (30*60*60*24) 
+      $months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));  
+        
+        
+      // To get the day, subtract it with years and  
+      // months and divide the resultant date into 
+      // total seconds in a days (60*60*24) 
+      $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24)); 
+        
+        
+      // To get the hour, subtract it with years,  
+      // months & seconds and divide the resultant 
+      // date into total seconds in a hours (60*60) 
+      $hours = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24) / (60*60));  
+        
+      // To get the minutes, subtract it with years, 
+      // months, seconds and hours and divide the  
+      // resultant date into total seconds i.e. 60 
+      $minutes = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60)/ 60);  
+        
+        
+      // To get the minutes, subtract it with years, 
+      // months, seconds, hours and minutes  
+      $seconds = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60 - $minutes*60));  
+        
+      // Print the result 
+      $calc_mins = sprintf("%d hours and %d minutes",$hours, $minutes);
+      // echo $excelrow['START_TIME'];
 
-$sql_items = mysqli_query($conn, "SELECT a.sn,a.id,a.procurement,pr.description,pr.unit,pr.qty,pr.abc FROM pr_items pr left join app a on a.id = pr.items WHERE pr.pr_no = '$pr_no' ");
+      
+    
+    
 
- 
-$row = 11;
-$rowA = 12;
-$rowB = 13;
-$rowC = 14;
-$rowD = 15;
-$rowE = 16;
-
-
-
-
-  while($excelrow = mysqli_fetch_assoc($sql_items) ){
-
-$unit = $excelrow['unit'];
-
-
-if ($unit == "1") {
-  $unit = "piece";
-}
-
-if ($unit == "2") {
-  $unit = "box";
-}
-
-if ($unit == "3") {
-  $unit = "ream";
-}
-
-if ($unit == "4") {
-  $unit = "lot";
-}
-
-if ($unit == "5") {
-  $unit = "unit";
-}
-
-if ($unit == "6") {
-  $unit = "crtg";
-}
-
-if ($unit == "7") {
-  $unit = "pack";
-}
-if ($unit == "8") {
-  $unit = "tube";
-}
-
-if ($unit == "9") {
-  $unit= "roll";
-}
-
-if ($unit == "10") {
-  $unit = "can";
-}
-
-if ($unit == "11") {
-  $unit = "bottle";
-}
-
-if ($unit == "12") {
-  $unit = "set";
-}
-
-if ($unit == "13") {
-  $unit = "jar";
-}
-
-if ($unit == "14") {
-  $unit = "bundle";
-}
-
-if ($unit == "15") {
-  $unit = "pad";
-}
-
-if ($unit == "16") {
-  $unit = "book";
-}
-
-if ($unit == "17") {
-  $unit = "pouch";
-}
-
-if ($unit == "18") {
-  $unit = "dozen";
-}
-
-if ($unit== "19") {
-  $unit = "pair";
-}
-
-if ($unit == "20") {
-  $unit = "gallon";
-}
-
-if ($unit == "21") {
-  $unit = "cart";
-}
-
-if ($unit == "22") {
-  $unit = "cart";
-}
+      
+        $requested_time = date('g:i A',strtotime($excelrow['START_DATE'].' '.$excelrow['REQ_TIME']));
+        $start_time     = date('g:i A',strtotime($excelrow['START_DATE'].' '.$excelrow['START_TIME']));
+        $completed_time = date('g:i A',strtotime($excelrow['COMPLETED_DATE'].' '.$excelrow['COMPLETED_TIME']));
 
 
-    $total = $excelrow['qty']*$excelrow['abc'];
-    $objPHPExcel->setActiveSheetIndex()->setCellValue('A'.$row,$excelrow['sn']);
-    $objPHPExcel->setActiveSheetIndex()->setCellValue('B'.$row,$unit);
-
-    $objPHPExcel->getActiveSheet()->getRowDimension($row)->setRowHeight(-1);
-    $objPHPExcel->setActiveSheetIndex()->setCellValue('C'.$row,$excelrow['procurement'] ."\n".$excelrow['description']);
-    $objPHPExcel->setActiveSheetIndex()->setCellValue('D'.$row,$excelrow['qty']);
-    $objPHPExcel->setActiveSheetIndex()->setCellValue('E'.$row,$excelrow['abc']);
-    //$objPHPExcel->setActiveSheetIndex()->setCellValue('F'.$row,number_format($total,2));
-    $objPHPExcel->setActiveSheetIndex()->setCellValue('F'.$row,$total);
-
-
-
-       // $objPHPExcel->setActiveSheetIndex()->setCellValue('G'.$row,$excelrow['abc']);
-    // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($stylebottom);
-    // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleTop);
-    // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleLeft);
-    // $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleRight);
-
-    // $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($stylebottom);
-    // $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($styleTop);
-    // $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($styleLeft);
-    // $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($styleRight);
-    // $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($styleRight);
-    // $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($stylebottom);
-
-    // $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($stylebottom);
-    // $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($styleTop);
-    // $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($styleLeft);
-    // $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($styleRight);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A'.$row,$no);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('B'.$row,$excelrow['CONTROL_NO']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('C'.$row,$excelrow['REQ_DATE']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('D'.$row,$requested_time);
+          $objPHPExcel->getActiveSheet(0)->mergeCells("E".$row.":F".$row);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('E'.$row,$excelrow['REQ_BY']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('G'.$row,$excelrow['OFFICE']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('H'.$row,$excelrow['ISSUE_PROBLEM']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('I'.$row,$excelrow['TYPE_REQ']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('J'.$row,$excelrow['ASSIST_BY']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('L'.$row,$excelrow['START_DATE']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('M'.$row,$start_time);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('N'.$row,$excelrow['COMPLETED_DATE']);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('O'.$row,$completed_time);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('P'.$row,$calc_mins);
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('Q'.$row,$excelrow['QUALITY']);
+          $objPHPExcel->getActiveSheet(0)->mergeCells("E11"."".":Q11");
+          $objPHPExcel->setActiveSheetIndex(0)->setCellValue('E11','Month of '.$excelrow['month'].' '.$year);
 
 
-    // $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($stylebottom);
-    // $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($styleTop);
-    // $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($styleLeft);
-    // $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($styleRight);
-    // $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($styleRight);
-    // $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($stylebottom);
-    $row++;
-    $rowA++;
-    $rowB++;
-    $rowC++;
-    $rowD++;
-    $rowE++;
+          $row++;
+          $no++;
+    }
   }
 
-if (mysqli_num_rows($sql_items)<10) {
 
-//  $counter++;
- 
-//  $z=15;
-//  for($i=0; $i<$z; $i++){
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('A'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('B'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('C'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('D'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('E'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('F'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('G'.$row,'');
-//   $objPHPExcel->setActiveSheetIndex()->setCellValue('H'.$row,'');
-
-//   $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('A'.$row)->applyFromArray($styleRight);
-
-//   $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->applyFromArray($styleLeft);
-//     // $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($styleRight);
-//     // $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($stylebottom);
-
-//   $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('C'.$row)->applyFromArray($styleRight);
-
-
-
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$row)->applyFromArray($styleRight);
-
-
-
-//   $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($styleRight);
-
-//   $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($styleRight);
-//   $objPHPExcel->getActiveSheet()->getStyle('E'.$row)->applyFromArray($stylebottom);
-//   $row++;
-//   $rowA++;
-//   $rowB++;
-//   $rowC++;
-//   $rowD++;
-//   $rowE++;
-
-//   if($i==15){
-
-//       $objPHPExcel->setActiveSheetIndex()->setCellValue('F'.$row,'test');
-
-
-//   }
-
-// }
-
-$select_purpsoe = mysqli_query($conn,"SELECT pr.purpose,pr.pmo,pmo.pmo_contact_person,pmo.designation FROM pr left join pmo on pmo.pmo_title = pr.pmo WHERE pr.id = '$id' ");
-$rowP = mysqli_fetch_array($select_purpsoe);
-$purpose = $rowP['purpose'];
-$pmo_contact_person = $rowP['pmo_contact_person'];
-$pmo_contact_person;
-$designation = $rowP['designation'];
-
-/* echo $pmo_contact_person;
-echo $designation; exit(); */
-
-
-
-$objPHPExcel->setActiveSheetIndex()->setCellValue('B37',$purpose);
-$objPHPExcel->setActiveSheetIndex()->setCellValue('B43',strtoupper($pmo_contact_person));
-$objPHPExcel->setActiveSheetIndex()->setCellValue('B44',$designation);
-
-
-
-// // $objPHPExcel->getActiveSheet()->mergeCells('B'.$row.':F'.$rowA);
-
-
-
-
-
-
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$row)->applyFromArray($styleRight);
-// $objPHPExcel->getActiveSheet()->getStyle('B'.$row)->getAlignment()->setWrapText(true); 
-
-// $objPHPExcel->getActiveSheet()->getStyle('A'.$rowA.':F'.$rowA)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$rowA)->applyFromArray($styleRight);
-
-
-
-
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('B'.$rowB,"Requested by:");
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('D'.$rowB,"Approved by:");
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('A'.$rowC,"Signature:");
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('A'.$rowD,"Printed Name:");
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('B'.$rowD,strtoupper($pmo_contact_person));
-// $objPHPExcel->getActiveSheet()->getStyle('B'.$rowD)->applyFromArray($styleHeader);
-// // $objPHPExcel->getActiveSheet()->mergeCells('B'.$rowC.':C'.$rowC);
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('D'.$rowD,"ELIAS F. FERNANDEZ, JR.");
-// $objPHPExcel->getActiveSheet()->getStyle('D'.$rowD)->applyFromArray($styleHeader);
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('A'.$rowE,"Designation:");
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('B'.$rowE,$designation);
-// $objPHPExcel->setActiveSheetIndex()->setCellValue('D'.$rowE,"OIC-Regional Director");
-
-// $objPHPExcel->getActiveSheet()->getStyle('A'.$rowE)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('B'.$rowE)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('C'.$rowE)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('D'.$rowE)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('E'.$rowE)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$rowE)->applyFromArray($stylebottom);
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$rowB)->applyFromArray($styleRight);
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$rowC)->applyFromArray($styleRight);
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$rowD)->applyFromArray($styleRight);
-// $objPHPExcel->getActiveSheet()->getStyle('F'.$rowE)->applyFromArray($styleRight);
-
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowD)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowD)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowD)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowD)->applyFromArray($styleRight);
-
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowB)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowB)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowB)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowB)->applyFromArray($styleRight);
-
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowC)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowC)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowC)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowC)->applyFromArray($styleRight);
-  
-
-  
-
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowE)->applyFromArray($stylebottom);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowE)->applyFromArray($styleTop);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowE)->applyFromArray($styleLeft);
-//   $objPHPExcel->getActiveSheet()->getStyle('D'.$rowE)->applyFromArray($styleRight);
-  
-
-  
-
-
-}
-    // $objPHPExcel->setActiveSheetIndex()->setCellValue('B'.$rowA,$excelrow['purpose']);
 
 
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
