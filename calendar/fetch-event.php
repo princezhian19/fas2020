5a839746df73407e23af42371806887febdf3ade
kim <markkimsacluti10101996@gmail.com>
2020-04-03 14:17:53 +0800

    close conn

--- calendar/fetch-event.php
+++ calendar/fetch-event.php
@@ -3,7 +3,6 @@
 
     $json = array();
     $sqlQuery = "SELECT id, title, start, end, color, cancelflag FROM events where cancelflag = 0 and status = 1";
-echo $sqlQuery;
     $result = mysqli_query($conn, $sqlQuery);
     $eventArray = array();
     while ($row = mysqli_fetch_assoc($result)) {
@@ -11,6 +10,5 @@ echo $sqlQuery;
     }
     mysqli_free_result($result);
 
-    mysqli_close($conn);
     echo json_encode($eventArray);
 ?>
