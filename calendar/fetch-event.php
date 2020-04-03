640f5d604cfc6b688d719ac23ff5319985f45233
kim <markkimsacluti10101996@gmail.com>
2020-04-03 14:22:33 +0800

    change code

--- calendar/fetch-event.php
+++ calendar/fetch-event.php
@@ -5,10 +5,12 @@
     $sqlQuery = "SELECT id, title, start, end, color, cancelflag FROM events where cancelflag = 0 and status = 1";
     $result = mysqli_query($conn, $sqlQuery);
     $eventArray = array();
-    while ($row = mysqli_fetch_assoc($result)) {
+    while ($row = mysqli_fetch_array($result)) {
         array_push($eventArray, $row);
     }
     mysqli_free_result($result);
 
     echo json_encode($eventArray);
+
+
 ?>
