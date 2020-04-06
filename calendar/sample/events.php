d5a9cbf5eeae995efebfb811f0114a33e40dc1d2
kim <markkimsacluti10101996@gmail.com>
2020-04-06 11:58:07 +0800

    commit april 06 calendar

---
+++ calendar/sample/events.php
@@ -0,0 +1,247 @@
+<?php
+session_start();
+require_once 'bdd.php';
+require_once 'dbaseCon.php';
+require_once 'sql_statements.php';
+
+$sql = "SELECT id, title, start, end, color, cancelflag FROM events where cancelflag = 0 and status = 1";
+
+$req = $bdd->prepare($sql);
+$req->execute();
+
+$events = $req->fetchAll();
+
+$currentuser=3174;
+
+
+
+
+
+$stmt = $bdd->query("SELECT * FROM tblemployee where EMP_N = '$currentuser'");
+while ($row = $stmt->fetch()) {
+	$lastname = $row['LAST_M'];
+	$firstname = $row['FIRST_M'];
+	$middlename = $row['MIDDLE_M'];
+	$isplanningofficer = $row['isPlanningOfficer'];
+
+	/*echo $isplanningofficer;*/
+
+
+	$name = $firstname . "\n" . $middlename . "\n" . $lastname;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>DILG Region IV-A LOOP</title>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- FullCalendar -->
+	<link href='fullcalendar/fullcalendar.min.css' rel='stylesheet' />
+
+
+	<!-- Custom CSS -->
+	<style>
+	body {
+		padding-top: 70px;
+		/* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+	}
+	#calendar {
+		max-width: 930px;
+	}
+	.col-centered{
+		float: none;
+		margin: 0 auto;
+	}
+</style>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Header -->
+</head>
+
+<body>
+
+
+	
+	<div style="max-width: 1500px;">
+		
+		<div class="row">
+			<div class="col-md-3 col-sm-12" style="margin-top: 3.5%; padding-left: 5%;">
+				<table class="table table-bordered" style="border-width: 3px;">
+					<tr>
+						<td style=""><b><center>DIVISION LEGENDS</center></b></td>
+					</tr>
+					<tr>
+						<td style="background-color: #D5D911; color:black;">ORD</td>
+					</tr>
+					<tr>
+						<td style="background-color: #0071c5; color:white;">LGMED</td>
+					</tr>
+					<tr>
+						<td style="background-color: #48BD0D; color:white;">LGCDD</td>
+					</tr>
+					<tr>
+						<td style="background-color: #8F0CC7; color:white;">PDMU</td>
+					</tr>
+					<tr>
+						<td style="background-color: #E6680E; color:white;">MBRTG</td>
+					</tr>
+					<tr>
+						<td style="background-color: #E60785; color:white;">FAD</td>
+					</tr>
+					<tr>
+						<td style="background-color: #000; color:white;">PROVINCE</td>
+					</tr>
+					
+				</table>
+			</div>
+
+			<div class="col-sm-10 col-md-9 text-center">
+				<div id="calendar" class="col-centered">
+				</div>
+			</div>
+
+
+			
+		</div>
+		
+	</div>
+
+
+	<!-- jQuery Version 1.11.1 -->
+	<script src="fullcalendar/lib/jquery.min.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+	
+	<!-- FullCalendar -->
+	<script src='fullcalendar/lib/moment.min.js'></script>
+	<script src='fullcalendar/fullcalendar.min.js'></script>
+	
+	<script>
+
+		$(document).ready(function() {
+
+			$('#calendar').fullCalendar({
+				header: {
+					left: 'prev,next today',
+					center: 'title',
+					right: 'month,basicWeek,basicDay'
+				},
+
+				editable: false,
+			eventLimit: true, // allow "more" link when too many events
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				
+				$('#ModalAdd #start').val(moment(start).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalAdd #end').val(moment(end).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalAdd').modal('show');
+
+			},
+			eventRender: function(event, element) {  
+				element.find('.fc-time').hide();
+			},
+
+			/*eventRender: function(event, element) {
+				element.bind('dblclick', function() {
+					$('#ModalEdit #id').val(event.id);
+					$('#ModalEdit #title').val(event.title);
+					$('#ModalEdit #color').val(event.color);
+					$('#ModalEdit').modal('show');
+				});
+			},*/
+			/*eventDrop: function(event, delta, revertFunc) { // si changement de position
+
+				edit(event);
+
+			},*/
+			/*eventResize: function(event,dayDelta,minuteDelta,revertFunc) { // si changement de longueur
+
+				edit(event);
+
+			},*/
+			events: [
+			<?php foreach($events as $event): 
+
+				$start = explode(" ", $event['start']);
+				$end = explode(" ", $event['end']);
+				if($start[1] == '00:00:00'){
+					$start = $start[0];
+				}else{
+					$start = $event['start'];
+				}
+				if($end[1] == '00:00:00'){
+					$end = $end[0];
+				}else{
+					$end = $event['end'];
+				}
+				if (TRUE) {
+					?>
+					{
+						id: '<?php echo $event['id']; ?>',
+						title: '<?php echo $event['title']; ?>',
+						start: '<?php echo $start; ?>',
+						end: '<?php echo $end; ?>',
+						color: '<?php echo $event['color']; ?>',
+						url: 'viewEvent.php?eventid=<?php echo $event['id']; ?>',
+
+					},
+				<?php } endforeach; ?>
+				]
+			});
+
+		/*function edit(event){
+			start = event.start.format('YYYY-MM-DD HH:mm:ss');
+			if(event.end){
+				end = event.end.format('YYYY-MM-DD HH:mm:ss');
+			}else{
+				end = start;
+			}
+			
+			id =  event.id;
+			
+			Event = [];
+			Event[0] = id;
+			Event[1] = start;
+			Event[2] = end;
+			
+			$.ajax({
+			 url: 'editEventDate.php',
+			 type: "POST",
+			 data: {Event:Event},
+			 success: function(rep) {
+					if(rep == 'OK'){
+						alert('Saved');
+					}else{
+						alert('Could not be saved. try again.'); 
+					}
+				}
+			});
+		}*/
+		
+	});
+
+</script>
+
+</body>
+
+</html>
