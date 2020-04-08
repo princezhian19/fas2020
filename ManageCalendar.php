807e34228f174376c9912f8451169a705c5f252b
kim <markkimsacluti10101996@gmail.com>
2020-04-08 17:08:16 +0800

    test

--- ManageCalendar.php
+++ ManageCalendar.php
@@ -1,4 +1,5 @@
-<?php session_start();
+<?php 
+session_start();
 if(!isset($_SESSION['username'])){
 header('location:index.php');
 }else{
@@ -7,63 +8,41 @@ ini_set('display_errors', 0);
 $username = $_SESSION['username'];
 $division = $_SESSION['division'];
 }
-
-
-require_once 'calendar/sample/bdd.php';
-require_once 'calendar/sample/dbaseCon.php';
-require_once 'calendar/sample/sql_statements.php';
-
-$sql = "SELECT id, title, start, end, color, cancelflag FROM events where cancelflag = 0 and status = 1";
-
-$req = $bdd->prepare($sql);
-$req->execute();
-
-$events = $req->fetchAll();
-
 ?>
 <!DOCTYPE html>
 <html>
-<title>FAS: Events Management Dashboard</title>
+
+<title>FAS Dashboard</title>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-  <link rel="shortcut icon" type="image/png" href="dilg.png">
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
   <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="_includes/sweetalert.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> </head>
-  <!-- CALENDAR -->
-  <link rel="stylesheet" href="calendar/fullcalendar/fullcalendar.min.css" />
-  <script src="calendar/fullcalendar/lib/jquery.min.js"></script>
-  <script src="calendar/fullcalendar/lib/moment.min.js"></script>
-  <script src="calendar/fullcalendar/fullcalendar.min.js"></script>
-<style> th{color:blue;}.response { height: 60px; } .success { background: #cdf3cd; padding: 10px 60px; border: #c3e6c3 1px solid; } </style>
+  <link href="_includes/sweetalert2.min.css" rel="stylesheet"/>
+  <link href="_includes/fontawesome.css" rel="stylesheet"/>
+ 
 
-<body >
-<?php 
-  if ($username == 'charlesodi' || $username == 'mmmonteiro' || $username == 'cvferrer' || $username == 'masacluti' || $username == 'magonzales' || $username == 'seolivar' || $username == 'jamonteiro' || $username == 'ctronquillo' || $username == 'rdmiranda') { include('sidebar.php'); }else{ include('sidebar2.php'); }
- ?>
-  <?php include('connection.php');?>
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-    <div class="content-wrapper">
-        <section class="content-header">
-            <ol class="breadcrumb"> <li><a href="home.php"><i class=""></i> Home</a></li> <li class="active">Events</li> </ol>
-                <div class="response"></div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="box">
-                                <div class="panel panel-defasult">
-                                    <div class="box-body"> 
-                                        <div>
-                                        <h1>Calendar of Activities:Manage All Events</h1><br>
-                                    </div>
-                                <div>
-                                <table id="example" class="table table-striped table-bordered table-responsive" style="width:;background-color: white;">
+<?php
+  include('sidebar.php'); 
+?>
+  
+  <div class="content-wrapper">
+    <section class="content-header">
+      <ol class="breadcrumb">
+        <li><a href="home.php"><i class=""></i> Home</a></li>
+        <li class="active">Technical Assistance Request Form</li>
+      </ol>
+      <br>
+      <br>
+      <table id="example" class="table table-striped table-bordered table-responsive" style="width:;background-color: white;">
                                 <thead>
                                 <th>NO.</th>
                                 <th>OFFICE</th>
@@ -75,24 +54,18 @@ $events = $req->fetchAll();
                                 <th style = "text-align:center;width:16%;">ACTION</th>
                                 </thead>
                                 </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
+    </section>
+  </div>
+ 
 </div>
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.j s"></script>
 <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
-<script src="dist/js/adminlte.min.js"></script>
 <script src="_includes/sweetalert.min.js"></script>
-
-
+<script src="_includes/sweetalert2.min.js"></script>
 <script>
 $(document).ready(function(){
     var table = $('#example').DataTable( {
@@ -127,6 +100,5 @@ $(document).ready(function(){
   
 });
 </script>
-
 </body>
 </html>
