19b7c91b4d740034aebd2ef909620a466b8533bb
charles <charlesodi1324@gmail.com>
2020-04-16 11:14:00 +0800

    app

--- app_table.php
+++ app_table.php
@@ -20,10 +20,43 @@
           <th width="150">HISTORY</th>
         </tr>
       </thead>
-    </table>
-  </div>
+      <?php 
+      $conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
+      $view_query = mysqli_query($conn, "
+        SELECT DISTINCT app.id,app.sn,app.code,ic.item_category_title,app.procurement,mop.mode_of_proc_title,pmo.pmo_title,sof.source_of_funds_title 
+        FROM app 
+        LEFT JOIN item_category ic on ic.id = app.category_id 
+        LEFT JOIN source_of_funds sof on sof.id = app.source_of_funds_id 
+        LEFT JOIN pmo on pmo.id = app.pmo_id 
+        LEFT JOIN mode_of_proc mop on mop.id = app.mode_of_proc_id 
+        ORDER BY app.id DESC ");
+      while ($row = mysqli_fetch_assoc($view_query)) {
+        $id = $row["id"];  
+        $sn = $row["sn"];  
+        $code = $row["code"];
+        $item_category_title = $row["item_category_title"];
+        $procurement = $row["procurement"];
+        $pmo_title = $row["pmo_title"];
+        $mode = $row["mode_of_proc_title"];
+        $source_of_funds_title = $row["source_of_funds_title"];
+        ?>
+        <tr>
+          <td width="1000"><?php echo $sn;?></td>
+          <td width="1000"><?php echo $code;?></td>
+          <td width="1000"><?php echo $item_category_title;?></td>
+          <td width="2000"><?php echo $procurement;?></td>
+          <td width="150"><?php echo $pmo_title;?></td>
+          <td width="1000"><?php echo $mode;?></td>
+          <td width="1000"><?php echo $source_of_funds_title;?></td>
+          <td width="150">
+           <a  href='UpdateAPP.php?id=<?php echo $id; ?>' title="Edit" class="btn btn-primary btn-xs"> <i class='fa'>&#xf044;</i>Edit</a>
+         </td>
+         <td><a  href='ViewApp_History.php?id=<?php echo $id; ?>' title="View" class="btn btn-info btn-xs"> <i class='fa'>&#xf06e;</i> History </a></td>
+       </tr>
+     <?php } ?>
+   </table>
+ </div>
 </div>
-<?php include('footer.php');?>
 </div>
 </div>
 
