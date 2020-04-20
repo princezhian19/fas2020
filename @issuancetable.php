cb75e8704ad2858585fc2e30ebc0d3045cd0a6b8
princezhian19 <zhianferrer019@gmail.com>
2020-04-20 13:51:27 +0800

    Commits updates April 20 Checking

--- @issuancetable.php
+++ @issuancetable.php
@@ -63,7 +63,7 @@
 
                ?>
 
-                <tr align = 'center'>
+                <tr align = ''>
             
                 
               
@@ -72,12 +72,33 @@
                 <td><?php echo $date_issued?></td>
                 <td><?php echo $subject?></td>
 
-                <td> 
-            
+                <td>
+
+
+                <?php   
+                            $conn = mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
+                            $username = $_SESSION['username'];
+
+                            //echo $username;
+                            $select_user = mysqli_query($conn,"SELECT DIVISION_C FROM tblemployee WHERE UNAME = '$username'");
+                            $rowdiv = mysqli_fetch_array($select_user);
+                            $DIVISION_C = $rowdiv['DIVISION_C'];
+                            //echo $DIVISION_C;
+                            
+                ?>
+
+              <?php if ($DIVISION_C == 15 || $DIVISION_C == 54 || $DIVISION_C == 16 ):?>
+                            
                 <a  href='ViewIssuance.php?id=<?php echo $id;?>' title="View" class = "btn btn-info btn-xs"> <i class='fa'>&#xf06e;</i> View</a> |
                 <a href='UpdateIssuances.php?id=<?php echo $id;?>'  class = "btn btn-primary btn-xs"> <i class='fa'>&#xf044;</i> Edit</a> | 
 
-                <a onclick="return confirm('Are you sure you want to Delete?');" name="del"  href="@Functions/issuancesdelete.php?id=<?php echo $id; ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete</a>
+                <a onclick="return confirm('Are you sure you want to delete this record?');" name="del"  href="@Functions/issuancesdelete.php?id=<?php echo $id; ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete</a>
+              <?php else :?>
+                              
+                <a  href='ViewIssuance.php?id=<?php echo $id;?>' title="View" class = "btn btn-info btn-xs"> <i class='fa'>&#xf06e;</i> View</a
+                 <?php endif?>
+            
+              
 
                 </td>
                 
@@ -104,8 +125,8 @@
         var dataTable=$('#example1').DataTable({
             'lengthChange': true,
             'searching'   : true,
-            'ordering'    : true,
-            'info'        : true,
+            'ordering'    : false,
+            'info'        : false,
             'autoWidth'   : true,
             "order": [[ 1, "asc" ]],
             aLengthMenu: [ [10, 20, -1], [ 10, 20, "All"] ],
--- issuances_update.php
+++ issuances_update.php
@@ -10,6 +10,110 @@ $username = $_SESSION['username'];
 
  
 ?>
+
+
+<?php
+
+
+if(isset($_POST['submit'])){
+
+
+    $filename = $_FILES['file']['name'];
+    $tempname = $_FILES['file']['tmp_name'];
+        
+    
+        if(isset($filename)){
+            if(!empty($filename)){
+    
+                $location = "files/";
+                if(move_uploaded_file($tempname, $location.$filename)){
+    
+                   
+    
+                        //echo 'File Uploaded!';
+                    
+                }
+               
+            }
+            
+            
+    
+        }
+    
+$id = $_POST['getid'];
+$category = $_POST['category'];
+$issuances = $_POST['issuances'];
+$dateissued1 = $_POST['dateissued'];
+
+$dateissued = date('Y-m-d', strtotime($dateissued1));
+$title = $_POST['title'];
+//$office = $_POST['office'];
+//$file = $_POST['file'];
+$url = $_POST['url'];
+$postedby = $_POST['postedby'];
+
+$posteddate = $_POST['posteddate'];
+
+/* echo $id; */
+
+/* echo $category;
+echo '<br>';
+echo $issuances;
+echo '<br>';
+echo $dateissued;
+echo '<br>';
+echo $title;
+echo '<br>';
+echo $offices;
+echo '<br>';
+echo $file;
+echo '<br>';
+echo $url;
+echo '<br>';
+echo $postedby;
+echo '<br>';
+echo $posteddate;
+exit();
+ */
+
+$servername = "localhost";
+$username = "fascalab_2020";
+$password = "w]zYV6X9{*BN";
+$database = "fascalab_2020";
+$username1 = $_SESSION['username'];
+$conn = new mysqli($servername, $username, $password,$database);
+
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+
+
+ $query = mysqli_query($conn,"UPDATE issuances set issuance_no='$issuances',status='approved',subject='$title',office_responsible='$postedby',pdf_file='$filename',dateposted='$posteddate',date_issued='$dateissued',postedby='$username1',category='$category',url='$url' where id ='$id'");
+/* echo "UPDATE issuances set issuance_no='$issuances',status='approved',subject='$title',office_responsible='$office',pdf_file='$file',dateposted='$posteddate',date_issued='$dateissued',postedby='$postedby',category='$category',url='$url' where id ='$id'";
+exit(); */
+
+mysqli_close($conn);
+
+if($query){
+
+    echo '<div class=""><div class="panel-heading " style = "background-color:Green"> <p style = "color:white;font-size:16px;"> Data has been successfully updated. </p> </div></div>  '; 
+    /* echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Data Updated Successfully!')
+    window.location.href='../issuances.php';
+    </SCRIPT>");  */
+
+}
+else{
+
+    echo '<div class=""><div class="panel-heading " style = "background-color:Red"> <p style = "color:white;font-size:16px;"> Error. </p> </div></div>  '; 
+    /* echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Error!')
+    window.location.href='../issuances.php';
+    </SCRIPT>"); */
+}
+}
+?>
 <!DOCTYPE html>
 
 <html>
@@ -114,7 +218,7 @@ $view_query = mysqli_query($conn, "SELECT * from issuances where id = '$getid'")
 
         <div class="class" >
 
-        <form method="POST" action='@Functions/issuancesupdate.php' enctype="multipart/form-data" >
+        <form method="POST" action='' enctype="multipart/form-data" >
        
         <input value="<?php echo $id;?>" hidden  type="text"  class="" style="height: 35px;" id="getid" placeholder="" name="getid">
 
@@ -280,7 +384,7 @@ $view_query = mysqli_query($conn, "SELECT * from issuances where id = '$getid'")
                             
                             </tr>
                     <tr>
-                        <td class="col-md-2"><label>Attached File</label> </td>
+                        <td class="col-md-2"><label>Attached File<span style = "color:red;">*</span></label> </td>
                             <td class="col-md-5"> <input value="<?php echo $file;?>" id="issuances_attachment" type="file" name="file"/>
                           <?php
 							if (!empty($_GET['option']) && $_GET['option']== 'edit') {
