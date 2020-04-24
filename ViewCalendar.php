6141ff4fb2f64ca2fe1f9746891eb4229b99b3c7
kim <markkimsacluti10101996@gmail.com>
2020-04-24 16:19:54 +0800

    calendar yea

--- ViewCalendar.php
+++ ViewCalendar.php
@@ -83,8 +83,7 @@ $events = $req->fetchAll();
       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
       <li class="active">Calendar of Events</li>
       </ol><br>
-     
-
+   
     <?php include 'calendar_view.php';?>
  &nbsp;
  &nbsp;
@@ -119,147 +118,126 @@ $events = $req->fetchAll();
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
 <script src="dist/js/adminlte.min.js"></script>
 <script>
-  $(function () {
-    $('#example2').DataTable()
-    $('#example1').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : true,
-      'ordering'    : false,
-      'info'        : true,
-      'autoWidth'   : true
-    })
-  })
-</script>
-	
-<script>
 $(document).ready(function() {
-  $('#calendar').fullCalendar({
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,basicWeek,basicDay'
-    },
-    editable: false,
-    eventLimit: true, // allow "more" link when too many events
-    selectable: true,
-    selectHelper: true,
-    select: function(start, end) {
-    $('#ModalAdd #start').val(moment(start).format('YYYY-MM-DD HH:mm:ss'));
-    $('#ModalAdd #end').val(moment(end).format('YYYY-MM-DD HH:mm:ss'));
-    $('#ModalAdd').modal('show');
-    },
-    eventRender: function(event, element) {  
-    element.find('.fc-time').hide();
-    },
-    eventDrop: function (event, delta) {
-                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                    $.ajax({
-                        url: 'edit-event.php',
-                        data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event.id,
-                        type: "POST",
-                        success: function (response) {
-                            displayMessage("Updated Successfully");
-                        }
-                    });
-                },
-  events: [
-  <?php foreach($events as $event): 
-
-    $start = explode(" ", $event['start']);
-    $end = explode(" ", $event['end']);
-    if($start[1] == '00:00:00'){
-      $start = $start[0];
-    }else{
-      $start = $event['start'];
-    }
-    if($end[1] == '00:00:00'){
-      $end = $end[0];
-    }else{
-      $end = $event['end'];
-    }
-
-
-  $enddate = str_replace('-', '/', $end);
-$realenddate = date('Y-m-d',strtotime($enddate . "+1 days"));
-
-if($_SESSION['planningofficer'] == 1){
-    if (TRUE) {
-      ?>
-      {
-        id: '<?php echo $event['id']; ?>',
-        title: '<?php echo $event['title']; ?>',
-        start: '<?php echo $start; ?>',
-        end: '<?php echo $realenddate; ?>',
-        color: '<?php echo $event['color']; ?>',
-        office: '<?php echo $event['office']; ?>',
-        url: 'ViewEvent.php?eventid=<?php echo $event['id']; ?>',
-
-      },
-    <?php 
-    }
-  }else{
-
-    if (TRUE) {
-      ?>
-      {
-        id: '<?php echo $event['id']; ?>',
-        title: '<?php echo $event['title']; ?>',
-        start: '<?php echo $start; ?>',
-        end: '<?php echo $realenddate; ?>',
-        color: '<?php echo $event['color']; ?>',
-        office: '<?php echo $event['office']; ?>',
-
-      },
-    <?php 
-    }
-  }
-  endforeach; ?>
-    ], 
-     eventRender: function eventRender( event, element, view ) {
-        return ['0', event.office].indexOf($('#selectDivision').val()) >= 0
-    }
-  });
-  $('#selectDivision').on('change',function(){
-    $('#calendar').fullCalendar('rerenderEvents');
-})
-
-
-
-
-/*function edit(event){
-  start = event.start.format('YYYY-MM-DD HH:mm:ss');
-  if(event.end){
-    end = event.end.format('YYYY-MM-DD HH:mm:ss');
-  }else{
-    end = start;
-  }
   
-  id =  event.id;
-  
-  Event = [];
-  Event[0] = id;
-  Event[1] = start;
-  Event[2] = end;
-  
-  $.ajax({
-   url: 'editEventDate.php',
-   type: "POST",
-   data: {Event:Event},
-   success: function(rep) {
-      if(rep == 'OK'){
-        alert('Saved');
-      }else{
-        alert('Could not be saved. try again.'); 
-      }
-    }
-  });
-}*/
+      $('#calendar').fullCalendar({
+          header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,basicWeek,basicDay'
+          },
+          editable: false,
+          eventLimit: true, // allow "more" link when too many events
+          selectable: true,
+          selectHelper: true,
+          eventRender: function(calEvent, element, view) {
+            if (calEvent.office == "0") {
+              // element.css('background-color', '#99FF99');
+            } if (calEvent.office == "1" ) {
+              // element.css('background-color', '#99FF99');
+            }
+            if (calEvent.office == "16") {
+              // element.css('background-color', '#415eec');
+            }
+            if (calEvent.office == "17") {
+              // element.css('background-color', '#D7CDD5');
+            }
+            if (calEvent.office == "7") {
+              // element.css('background-color', '#D7CDD5');
+            }
+            if (calEvent.office == "18") {
+              // element.css('background-color', '#D7CDD5');
+            }
+            if (calEvent.office == "9") {
+              // element.css('background-color', '#D7CDD5');
+            }
+            if($('input[id=all]').is(':checked')){
+              return ['0', calEvent.office].indexOf($('#selectDivision').val()) >= 0  
 
-});
+            }else{
+              return filter(calEvent); // Only show if appropriate checkbox is checked
 
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];;
+            }
+          },
+  
+          events: [
+                  <?php foreach($events as $event): 
+
+                    $start = explode(" ", $event['start']);
+                    $end = explode(" ", $event['end']);
+                    if($start[1] == '00:00:00'){
+                      $start = $start[0];
+                    }else{
+                      $start = $event['start'];
+                    }
+                    if($end[1] == '00:00:00'){
+                      $end = $end[0];
+                    }else{
+                      $end = $event['end'];
+                    }
+
+
+                  $enddate = str_replace('-', '/', $end);
+                  $realenddate = date('Y-m-d',strtotime($enddate . "+1 days"));
+
+                  if($_SESSION['planningofficer'] == 1){
+                    if (TRUE) {
+                      ?>
+                      {
+                        id: '<?php echo $event['id']; ?>',
+                        title: '<?php echo $event['title']; ?>',
+                        start: '<?php echo $start; ?>',
+                        end: '<?php echo $realenddate; ?>',
+                        color: '<?php echo $event['color']; ?>',
+                        office: '<?php echo $event['office']; ?>',
+                        url: 'ViewEvent.php?eventid=<?php echo $event['id']; ?>'
+
+                      },
+                    <?php 
+                    }
+                  }else{
+
+                    if (TRUE) {
+                      ?>
+                      {
+                        id: '<?php echo $event['id']; ?>',
+                        title: '<?php echo $event['title']; ?>',
+                        start: '<?php echo $start; ?>',
+                        end: '<?php echo $realenddate; ?>',
+                        color: '<?php echo $event['color']; ?>',
+                        office: '<?php echo $event['office']; ?>'
+
+                      },
+                    <?php 
+                    }
+                  }
+                  endforeach; ?>
+                ]
+      });
+      // ===================================================
+      $('#selectDivision').hide();
+      
+      // ===================================================
+      $(".select_month").on("change", function(event) {
+      $('#calendar').fullCalendar('changeView', 'month', this.value);
+      $('#calendar').fullCalendar('gotoDate', "2020-"+this.value+"-1");
+      });
+
+   
+      
+  // $('.calFilter').on('change', function() {
+  //     $('.calFilter').not(this).prop('checked', false);  
+  // });
+      $('input[type=radio][name=user_selector]').on('change', function() {
+        $('#calendar').fullCalendar('rerenderEvents');
+      });
+      /* When a checkbox changes, re-render events */
+      $('input:checkbox.calFilter').on('change', function() {
+        $('#calendar').fullCalendar('rerenderEvents');
+      });
+    });
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];;
 var date = new Date();
 var month = months[date.getMonth()];
 switch(month)
@@ -301,111 +279,14 @@ switch(month)
       $("#selectMonth").append('<select class="select_month form-control"><option value="1">January</option><option value="2">February</option><option value="3">March</option><option value="4">April</option><option value="5">May</option><option value="6">June</option><option value="7">July</option><option value="8">August</option><option value="9">September</option><option value="10">October</option><option value="11">November</option><option value="12" selected>December</option></select>');
     break;
 }
-
-$(".select_month").on("change", function(event) {
-$('#calendar').fullCalendar('changeView', 'month', this.value);
-$('#calendar').fullCalendar('gotoDate', "2020-"+this.value+"-1");
-
-});
-function displayMessage(message) {
-  $(".response").html("<div class='success'>"+message+"</div>");
-setInterval(function() { $(".success").fadeOut(); }, 1000);
-}
-
-</script>
-
-<script>
-/*
-     * LetterAvatar
-     * 
-     * Artur Heinze
-     * Create Letter avatar based on Initials
-     * based on https://gist.github.com/leecrossley/6027780
-     */
-     (function(w, d){
-
-
-function LetterAvatar (name, size) {
-
-    name  = name || '';
-    size  = size || 60;
-
-    var colours = [
-            "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", 
-            "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"
-        ],
-
-        nameSplit = String(name).toUpperCase().split(' '),
-        initials, charIndex, colourIndex, canvas, context, dataURI;
-
-
-    if (nameSplit.length == 1) {
-        initials = nameSplit[0] ? nameSplit[0].charAt(0):'?';
-    } else {
-        initials = nameSplit[0].charAt(0) + nameSplit[1].charAt(0);
+    function filter(calEvent) {
+      var vals = [];
+      $('input:checkbox.calFilter:checked').each(function() {
+        vals.push($(this).val());
+      });
+      return vals.indexOf(calEvent.office) !== -1;
     }
 
-    if (w.devicePixelRatio) {
-        size = (size * w.devicePixelRatio);
-    }
-        
-    charIndex     = (initials == '?' ? 72 : initials.charCodeAt(0)) - 64;
-    colourIndex   = charIndex % 20;
-    canvas        = d.createElement('canvas');
-    canvas.width  = size;
-    canvas.height = size;
-    context       = canvas.getContext("2d");
-     
-    context.fillStyle = colours[colourIndex - 1];
-    context.fillRect (0, 0, canvas.width, canvas.height);
-    context.font = Math.round(canvas.width/2)+"px Arial";
-    context.textAlign = "center";
-    context.fillStyle = "#FFF";
-    context.fillText(initials, size / 2, size / 1.5);
-
-    dataURI = canvas.toDataURL();
-    canvas  = null;
-
-    return dataURI;
-}
-
-LetterAvatar.transform = function() {
-
-    Array.prototype.forEach.call(d.querySelectorAll('img[avatar]'), function(img, name) {
-        name = img.getAttribute('avatar');
-        img.src = LetterAvatar(name, img.getAttribute('width'));
-        img.removeAttribute('avatar');
-        img.setAttribute('alt', name);
-    });
-};
-
-
-// AMD support
-if (typeof define === 'function' && define.amd) {
-    
-    define(function () { return LetterAvatar; });
-
-// CommonJS and Node.js module support.
-} else if (typeof exports !== 'undefined') {
-    
-    // Support Node.js specific `module.exports` (which can be a function)
-    if (typeof module != 'undefined' && module.exports) {
-        exports = module.exports = LetterAvatar;
-    }
-
-    // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
-    exports.LetterAvatar = LetterAvatar;
-
-} else {
-    
-    window.LetterAvatar = LetterAvatar;
-
-    d.addEventListener('DOMContentLoaded', function(event) {
-        LetterAvatar.transform();
-    });
-}
-
-})(window, document);
 </script>
 
 </body>
--- calendar_view.php
+++ calendar_view.php
@@ -3,15 +3,12 @@
           <div class="box">
             <div class="box-body">
               <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                 <h1>Calendar of Events</h1>
-                
-                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-12" id = "selectMonth" >  
+                <div class = "col-lg-12">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-6" id = "selectMonth" >  
                     </div>
-                    <br>
-                    <br>
-                    <br>
-                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-12">
+                    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-6">
                         <select class="form-control " id="selectYear" style="width: 100%;">
                             <?php 
                             for($i= 2020; $i < 2031; $i++)
@@ -20,7 +17,7 @@
                             }
                             ?>
                             </select>
-                            </div>  <br><br><br>
+                            </div> 
                             <div class="col-md-12">
                             <?php 
                             require_once 'connection.php';
@@ -40,6 +37,10 @@
                         ?>
                         
                     </div>
+</div>
+                    
+                    
+                    
                     <br><br><br>
                     <div class="col-xs-2 col-sm-2 col-md-2 col-lg-12">
                     <button class="col-lg-12 pull-right btn btn-success"  ><i class = "fa fa-plus"></i><a style = "color:#fff;" href = "calAddEvents.php"> Add Events</a></button>
@@ -54,39 +55,101 @@
                     <br>
                     <div class="col-xs-2 col-sm-2 col-md-2 col-lg-12">
                     <button class="col-lg-12 pull-right btn btn-success" style = "margin-left:10px;"><a style = "color:#fff;"  href="export_calendar.php?date=<?php echo date("Y-m-d");?>&division=<?php echo $_SESSION['division'];?>" >Export</a></button>
-                
+  
+                    
                     </div>
                     <br>
                     <br>
-                    <table class="table table-bordered" style="border-width: 3px;">
+                        <div class = "col-lg-6">
+                        <table class="table table-bordered" style="border-width: 3px;max-width:100%;">
 					<tr>
-						<td style=""><b><center>DIVISION LEGENDS</center></b></td>
+						<td style=""><b><center>REGION</center></b></td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <input  class='calFilter' type="checkbox" value="0" checked id = "all"></label>
+                            Show all
+                        </td>
+                    </tr>
+					<tr>
+                        <td style="background-color: #D5D911; color:white;">
+                            <input class='calFilter' type="checkbox" value="1" >ORD</label>
+                        </td>
 					</tr>
 					<tr>
-						<td style="background-color: #D5D911; color:black;">ORD</td>
+                    <td style="background-color: #E60785; color:white;">
+                    <input class='calFilter' type="checkbox" value="16">FAD</label>
+                        
+                    </td>
+                       
 					</tr>
 					<tr>
-						<td style="background-color: #0071c5; color:white;">LGMED</td>
+                        <td style="background-color: #48BD0D; color:white;">
+                        <input class='calFilter' type="checkbox" value="17" >LGCDD</label>
+
+                        </td>
 					</tr>
 					<tr>
-						<td style="background-color: #48BD0D; color:white;">LGCDD</td>
+                    <td style="background-color: #E6680E; color:white;">
+                        <input class='calFilter' type="checkbox" value="7">MBRTG</label>
+                    </td>
+                       
 					</tr>
 					<tr>
-						<td style="background-color: #8F0CC7; color:white;">PDMU</td>
+                    <td style="background-color: #0071c5; color:white;">
+                    <input class='calFilter' type="checkbox" value="18">LGMED</label>
+
+                        </td>
+                    
 					</tr>
 					<tr>
-						<td style="background-color: #E6680E; color:white;">MBRTG</td>
-					</tr>
-					<tr>
-						<td style="background-color: #E60785; color:white;">FAD</td>
-					</tr>
-					<tr>
-						<td style="background-color: #000; color:white;">PROVINCE</td>
+                    <td style="background-color: #8F0CC7; color:white;">
+                    <input class='calFilter' type="checkbox" value="9">PDMU</label>
+
+                        </td>
 					</tr>
 					
-				</table>
+					
+                </table>
+                        </div>
+                        <div class = "col-lg-6">
+                        <table class="table table-bordered" style="border-width: 3px;max-width:100%;">
+					<tr>
+						<td style=""><b><center>PROVINCE/HUC</center></b></td>
+                    </tr>
+                    <tr>
+                    <td>
+                        <input  class='calFilter' type="checkbox" value="0"  id = "addll"></label>
+                            Show all
+                        </td>
+                    </tr>
+					<tr>
+						<td style="background-color: #D5D911; color:black;padding:9px;">Batangas</td>
+					</tr>
+					<tr>
+						<td style="background-color: #0071c5; color:white;;padding:9px;">Cavite</td>
+					</tr>
+					<tr>
+						<td style="background-color: #48BD0D; color:white;;padding:9px;">Laguna</td>
+					</tr>
+					<tr>
+						<td style="background-color: #8F0CC7; color:white;;padding:9px;">Rizal</td>
+					</tr>
+					<tr>
+						<td style="background-color: #E6680E; color:white;;padding:9px;">Quezon</td>
+					</tr>
+					<tr>
+						<td style="background-color: #E60785; color:white;;padding:9px;">Lucena</td>
+					</tr>
+				
+                </table>
+                     
+                    </div>
+
+                    
+                
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-8">
                     
                     
                     
