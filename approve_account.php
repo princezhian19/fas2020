84e192dafa871f90885814dcdf35656a952ddfed
charles <charlesodi1324@gmail.com>
2020-04-27 16:38:05 +0800

    asd

--- approve_account.php
+++ approve_account.php
@@ -2,34 +2,35 @@
   <div class="col-md-12">
     <div class="box">
       <div class="box-body"> 
-        <h3 align="">For Approval(s)</h3>
+        <h3 align="">Account Approval</h3>
         <br>
         <br>
-        <table id="example1" class="table table-striped table-bordered" style="background-color: white;">
-          <thead>
+        <table id="example1" class="table table-striped table-bordered" style="background-color: white;" >
+          <thead >
             <tr style="background-color: white;color:blue;">
-              <th width="150">USERNAME</th>
-              <th width="150">LAST NAME</th>
+              <th width="150">OFFICE</th>
               <th width="150">FIRST NAME</th>
               <th width="150">MIDDLE NAME</th>
+              <th width="150">LAST NAME</th>
               <th width="180">ACTION</th>
             </tr>
           </thead>
           <?php
           $conn = mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
-          $QUERY = mysqli_query($conn, "SELECT UNAME,EMP_N,LAST_M,MIDDLE_M,FIRST_M FROM tblemployee WHERE ACTIVATED = 'No' AND BLOCK ='N' ORDER BY EMP_N DESC ");
+          $QUERY = mysqli_query($conn, "SELECT tblpersonneldivision.DIVISION_M,tblemployee.UNAME,tblemployee.EMP_N,tblemployee.LAST_M,tblemployee.MIDDLE_M,tblemployee.FIRST_M FROM tblemployee LEFT JOIN tblpersonneldivision on tblpersonneldivision.DIVISION_N = tblemployee.DIVISION_C WHERE tblemployee.ACTIVATED = 'No' AND tblemployee.BLOCK ='N' ORDER BY tblemployee.EMP_N DESC ");
           while ($row = mysqli_fetch_assoc($QUERY)) {
             $id = $row["EMP_N"];
+            $DIVISION_M = $row["DIVISION_M"];
             $UNAME = $row["UNAME"];
             $LAST_M = $row["LAST_M"];
             $FIRST_M = $row["FIRST_M"];  
             $MIDDLE_M = $row["MIDDLE_M"];
             ?>
             <tr>
-              <td><?php echo $UNAME;?></td>
-              <td><?php echo $LAST_M;?></td>
+              <td><?php echo $DIVISION_M;?></td>
               <td><?php echo $FIRST_M;?></td>
               <td><?php echo $MIDDLE_M;?></td>
+              <td><?php echo $LAST_M;?></td>
               <td>
                 <a onclick="return confirm('Are you sure you want to Activated this Account now?');" href='approve.php?id=<?php echo $id;?>&username=<?php echo $username;?>' title="activate" class = "btn btn-success btn-xs" > <i class='fa fa-fw fa-check-square-o'></i> Approve</a>  
               </td>
--- create_directory.php
+++ create_directory.php
@@ -8,8 +8,10 @@ if (isset($_POST['submit'])) {
   $contact =$_POST['contact'];
   $email =$_POST['email'];
   $address =$_POST['address'];
+  $posted_by =$_POST['posted_by'];
+  $posted_date =$_POST['posted_date'];
 
-  $INSERT = mysqli_query($conn,"INSERT INTO `phone_directory`(`group`, `agency`, `head_director`, `contact_no`, `email`, `address`) VALUES ('$group','$agency','$head_director','$contact','$email','$address')");
+  $INSERT = mysqli_query($conn,"INSERT INTO `phone_directory`(`group`, `agency`, `head_director`, `contact_no`, `email`, `address`, `posted_by`, `posted_date`) VALUES ('$group','$agency','$head_director','$contact','$email','$address','$posted_by','$posted_date')");
 
   if ($INSERT) {
       echo ("<SCRIPT LANGUAGE='JavaScript'>
@@ -28,7 +30,7 @@ if (isset($_POST['submit'])) {
 ?>
 <div class="box">
   <div class="box-body">
-    <h1 align="">&nbspAdd Phone Directory</h1>
+    <h1 align="">&nbspAdd Contact</h1>
     <br>
     &nbsp &nbsp &nbsp   <li class="btn btn-success"><a href="Directory.php" style="color:white;text-decoration: none;">Back</a></li>
     <br>
@@ -40,39 +42,47 @@ if (isset($_POST['submit'])) {
             <div class="col-md-6">
               <div class="form-group">
                 <label>Group <label style="color: Red;" >*</label></label>
-                <select class="form-control select2"  name="group" >
+                <select required class="form-control select2"  name="group" >
                   <option disabled selected></option>
                   <option value="Local Government Units">Local Government Units</option>
-                  <option value="League of Mayors">League of Mayors</option>
+                  <option value="DILG Central">DILG Central</option>
+                  <option value="DILG Region">DILG Region</option>
                   <option value="Private Sector Representatives">Private Sector Representatives</option>
                   <option value="Regional Line Agencies">Regional Line Agencies</option>
-                  <option value="NON-VOTING RLAs">NON-VOTING RLAs</option>
                   <option value="States, Universities and Colleges">States, Universities and Colleges </option>
                   <option value="REGULAR GUESTS">REGULAR GUESTS </option>
                 </select>
               </div>
               <div class="form-group">
-                <label>Agency <label style="color: Red;" >*</label></label>
-                <input type="text" class="form-control"  name="agency"  >
+                <label>Agency/Office <label style="color: Red;" >*</label></label>
+                <input required type="text" class="form-control"  name="agency"  >
               </div>
               <div class="form-group">
-                <label>Head/Director <label style="color: Red;" >*</label></label>
-                <input  class="form-control" name="head_director" type="text" >
+                <label>Contact/Person <label style="color: Red;" >*</label></label>
+                <input required class="form-control" name="head_director" type="text" >
+              </div>
+              <div class="form-group">
+                <label>Posted by <label style="color: Red;" >*</label></label>
+                <input required class="form-control" name="posted_by" type="text" >
               </div>
             </div>
             <div class="col-md-6">
              <div class="form-group">
               <label>Contact No. <label style="color: Red;" >*</label></label>
-              <input  class="form-control" name="contact" type="text" >
+              <input required class="form-control" name="contact" type="text" >
             </div>
             <div class="form-group">
-              <label>Email <label style="color: Red;" >*</label></label>
+              <label>Email Address</label>
               <input  class="form-control" name="email" type="text" >
             </div>
             <div class="form-group">
-              <label>Address <label style="color: Red;" >*</label></label>
+              <label>Office Address</label>
               <input  class="form-control" name="address" type="text" >
             </div>
+            <div class="form-group">
+                <label>Posted Date <label style="color: Red;" >*</label></label>
+                <input required class="form-control" name="posted_date" type="date" >
+              </div>
           </div>
         </div>
       </div>
