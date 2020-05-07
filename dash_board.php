1a478ce59897e3bd36a08a373102a06a579c7fed
charles <charlesodi1324@gmail.com>
2020-05-07 11:20:57 +0800

    asd

--- dash_board.php
+++ dash_board.php
@@ -422,7 +422,7 @@
              <div class="clearfix"></div>
            </div>
            <div style="padding-left: 10px;padding-right: 10px;background:#ee5;">
-             <img class="direct-chat-img" src="images/logo.png" alt="message user image"><a href="">Charles Adrian T. Odi</a>
+             <img class="direct-chat-img" src="images/LOGO.png" alt="message user image"><a href="">Charles Adrian T. Odi</a>
              <p></p>
 
              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
