572a1651de65b9090fec2ccc3513399e4fd61511
charles <charlesodi1324@gmail.com>
2020-05-12 11:04:31 +0800

    asd

--- delete_announcement.php
+++ delete_announcement.php
@@ -2,9 +2,9 @@
 $id = $_GET['id'];
 $username = $_GET['username'];
 $conn = mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
-$delete = mysqli_query($conn,"DELETE FROM announcement WHERE id = $id");
+$delete = mysqli_query($conn,"DELETE FROM announcementt WHERE id = $id");
 
-if ($activate) {
+if ($delete) {
 	if ($username == 'charlesodi' || $username == 'mmmonteiro' || $username == 'jamonteiro' || $username == 'rlsegunial' || $username == 'masacluti' || $username == 'cvferrer' || $username == 'seolivar' || $username == 'magonzales') {
 		echo ("<SCRIPT LANGUAGE='JavaScript'>
     window.alert('Successfuly Deleted!');
