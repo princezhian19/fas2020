7c79a0c245db4288da372b7c80c8e5342ac5ee94
kim <markkimsacluti10101996@gmail.com>
2020-05-18 12:15:45 +0800

    fixxxx

--- sidebar2.php
+++ sidebar2.php
@@ -12,7 +12,7 @@ $username = $_SESSION['username'];
         function getDivision()
         {
         include 'connection.php';
-        $sqlUsername = mysqli_query($conn,"SELECT * FROM  tblemployeeinfo INNER JOIN tblpersonneldivision ON tblemployeeinfo.DIVISION_C = tblpersonneldivision.DIVISION_N where  UNAME ='".$_SESSION['division']."'");
+        $sqlUsername = mysqli_query($conn,"SELECT * FROM  tblemployeeinfo INNER JOIN tblpersonneldivision ON tblemployeeinfo.DIVISION_C = tblpersonneldivision.DIVISION_N where  UNAME ='".$_SESSION['username']."'");
         $row = mysqli_fetch_array($sqlUsername);
         echo  $row['DIVISION_M']; 
         }
