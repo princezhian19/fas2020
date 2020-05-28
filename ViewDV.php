84f0905c865ecf5e3cdf9922d940b907b093a931
charles <charlesodi1324@gmail.com>
2020-05-28 10:08:41 +0800

    asd

--- ViewDV.php
+++ ViewDV.php
@@ -44,11 +44,7 @@ $username = $_SESSION['username'];
 <body class="hold-transition skin-red-light sidebar-mini">
 <div class="wrapper">
 <?php 
-  if ($division == 14 || $division == 10 || $division == 11 || $division == 12 || $division == 13) {
-      include('test1.php');
-    }else{
-      include('sidebar2.php');
-    }
+    if ($username == 'charlesodi' || $username == 'mmmonteiro' || $username == 'cvferrer' || $username == 'masacluti' || $username == 'magonzales' || $username == 'seolivar' || $username == 'jamonteiro' || $username == 'ctronquillo' || $username == 'rdmiranda') { include('test1.php'); }else{ include('sidebar2.php'); }
  ?>
   <div class="content-wrapper">
     <section class="content-header">
