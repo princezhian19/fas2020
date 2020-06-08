f064b229a4af96129a20e2deb77c3fd6c56447bd
charlesodi <charlesodi1324@gmail.com>
2020-06-08 12:15:51 +0800

    asd

--- daily_logs.php
+++ daily_logs.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Manila');
 $conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
 $u = mysqli_query($conn,"SELECT emp.FIRST_M,emp.MIDDLE_M,emp.LAST_M,pos.POSITION_M FROM tblemployeeinfo emp LEFT JOIN tbldilgposition pos on pos.POSITION_ID = emp.POSITION_C WHERE emp.UNAME = '$username' ");
 $row = mysqli_fetch_array($u);
