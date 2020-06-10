c9be4613b55352996589d460042e1e9644b525b0
charles <charlesodi1324@gmail.com>
2020-06-10 16:37:39 +0800

    aaa

--- processing.php
+++ processing.php
@@ -286,7 +286,7 @@ function filldataTable()
                     }
                 }else{
                     ?>
-                <button    class = "btn btn-danger btn-md col-lg-12 ">Rate Service<a href = "rateService.php?division=<?php echo $_GET['division'];?>&id=<?php echo $row['CONTROL_NO'];?>" style = "decoration:none;color:#fff;" >Rate Service</a></button>
+                <button    class = "btn btn-danger btn-md col-lg-12 "><a href = "rateService.php?division=<?php echo $_GET['division'];?>&id=<?php echo $row['CONTROL_NO'];?>" style = "decoration:none;color:#fff;" >Rate Service</a></button>
 <?php
                 }
 
