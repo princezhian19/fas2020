a2489c612a5d3703fe782a35ce1c9aab4e3917a0
charles <charlesodi1324@gmail.com>
2020-06-10 14:52:48 +0800

    aa

--- monitoring.php
+++ monitoring.php
@@ -56,7 +56,7 @@ $division = $_GET['division'];
 <div class="wrapper">
 <?php 
  if($_GET['division'] == 10 || $_GET['division'] == 11 || $_GET['division'] == 12 || $_GET['division'] == 13 || $_GET['division'] == 14 || $_GET['division'] == 16)
-
+{
       include('test1.php');
     }else{
       include('sidebar2.php');
