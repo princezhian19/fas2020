551f709c53c7abd22ca33ba07baeef32f10a49ee
charlesodi <charlesodi1324@gmail.com>
2020-06-11 23:26:28 +0800

    asd

--- home2.php
+++ home2.php
@@ -5,40 +5,67 @@ header('location:index.php');
   error_reporting(0);
 ini_set('display_errors', 0);
 $username = $_SESSION['username'];
+$division = $_SESSION['division'];
 }
 ?>
 <!DOCTYPE html>
 <html>
-<title>Dashboard</title>
+<title>FAS | Dashboard</title>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="shortcut icon" type="image/png" href="dilg.png">
-  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+        folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="bower_components/morris.js/morris.css">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
-  <div class="wrapper">
-    <?php include('sidebar3.php');?>
-
-      <div class="content-wrapper">
-        <section class="content-header">
-          <ol class="breadcrumb">
-            <li><a href="home.php"><i class=""></i> Home</a></li>
-            <li class="active"></li>
-          </ol>
+<body class="hold-transition skin-red-light sidebar-mini">
+<div class="wrapper">
+  <?php 
+      include('sidebar3.php');
+ ?>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <ol class="breadcrumb">
+        <li><a href="home.php"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
       <br>
       <br>
-        <?php include('dash_board1.php');?>
-
+        <?php include('dash_board2.php');?>
     </section>
   </div>
- 
+  <footer class="main-footer">
+  <br>
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+      </div>
+      <strong>DILG IV-A Regional Information and Communications Technology Unit (RICTU) © 2019 All Right Reserved .</strong> 
+    </footer>
+    <br>
 </div>
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -47,14 +74,19 @@ $username = $_SESSION['username'];
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
 <script src="dist/js/adminlte.min.js"></script>
-<script src="dist/js/demo.js"></script>
+<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+
+
+
+
+
 <script>
   $(function () {
     $('').DataTable()
     $('#example1').DataTable({
       'paging'      : true,
       'lengthChange': true,
-      'searching'   : true,
+      'searching'   : false,
       'ordering'    : false,
       'info'        : true,
       'autoWidth'   : true,
@@ -74,7 +106,7 @@ $username = $_SESSION['username'];
     $('#example2').DataTable({
       'paging'      : true,
       'lengthChange': true,
-      'searching'   : true,
+      'searching'   : false,
       'ordering'    : false,
       'info'        : true,
       'autoWidth'   : true,
@@ -94,7 +126,7 @@ $username = $_SESSION['username'];
     $('#example3').DataTable({
       'paging'      : true,
       'lengthChange': true,
-      'searching'   : true,
+      'searching'   : false,
       'ordering'    : false,
       'info'        : true,
       'autoWidth'   : true,
@@ -115,10 +147,36 @@ $username = $_SESSION['username'];
     $('#example4').DataTable({
       'paging'      : true,
       'lengthChange': true,
-      'searching'   : true,
+      'searching'   : false,
       'ordering'    : false,
       'info'        : true,
       'autoWidth'   : true
+    })
+  })
+</script>
+
+<script>
+  $(function () {
+    $('#example15').DataTable({
+      'paging'      : true,
+      'lengthChange': true,
+      'searching'   : false,
+      'ordering'    : false,
+      'info'        : true,
+      'autoWidth'   : true,
+      aLengthMenu: [ [1, 10, 20, -1], [1, 10, 20, "All"] ],
+      "bPaginate": false,
+      "bLengthChange": false,
+      "bFilter": true,
+      "bInfo": false,
+      "bAutoWidth": false,
+      "pagingType": "simple",
+      "language": {
+      "paginate": {
+      "previous": "<",
+      "next":">"
+}
+}
     })
   })
 </script>
