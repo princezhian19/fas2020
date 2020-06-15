20ee8a90178068b13f3427f4829dff8a73c1c240
charles <charlesodi1324@gmail.com>
2020-06-15 09:49:51 +0800

    aaaa

--- ViewCalendar.php
+++ ViewCalendar.php
@@ -278,11 +278,18 @@ if($_SESSION['planningofficer'] == 1)
   }
     </style>
   <?php 
-  if ($division == 14 || $division == 10 || $division == 11 || $division == 12 || $division == 13) {
-      include('test1.php');
-    }else{
-      include('sidebar2.php');
-    }
+if ($username == 'charlesodi' || $username == 'itdummy1' || $username == 'mmmonteiro' || $username == 'rlsegunial' || $username == 'masacluti' || $username == 'cvferrer' || $username == 'seolivar') {
+  include('test1.php');
+
+}else{
+  include('sidebar2.php');
+
+}
+  // if ($division == 14 || $division == 10 || $division == 11 || $division == 12 || $division == 13) {
+  //     include('test1.php');
+  //   }else{
+  //     include('sidebar2.php');
+  //   }
  ?>
 <?php include 'connection.php';?>
 
