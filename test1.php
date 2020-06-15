e0b33444252aec7fe6d6d9fd34fd7f71643ddec5
charles <charlesodi1324@gmail.com>
2020-06-15 10:16:15 +0800

    aaaa

--- test1.php
+++ test1.php
@@ -648,10 +648,7 @@ function getImage()
 
 
 
-            <a href="processing.php?division=<?php echo $_SESSION['division'];?>&ticket_id=" >
-                <i class="fa fa-users" style = "color:#black;"></i>
-                <span  style = "color:#black;font-weight:normal;">ICT Technical Assistance</span>
-            </a>
+           
            
         </li>
       
