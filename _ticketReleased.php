559aaa46103cf4f3d622f6fc3d53a19c1c6c20dc
charles <charlesodi1324@gmail.com>
2020-06-16 09:41:50 +0800

    TRY

--- _ticketReleased.php
+++ _ticketReleased.php
@@ -15,8 +15,7 @@ $time_recieved= date("H:i", strtotime($time));
 
 switch ($option) {
     case 'released':
-        $query = "SELECT * FROM `ta_monitoring` 
-        where `STATUS_REQUEST` = 'RECEIVED' ";
+        $query = "SELECT * FROM `ta_monitoring` WHERE `STATUS_REQUEST` LIKE '%RECEIVED%' ";
 
         $result = mysqli_query($conn, $query);
         $COUNT = '';
@@ -24,7 +23,7 @@ switch ($option) {
         {
             $COUNT = $row['COUNT']+1;
         }
-
+// =============================================================
         $insert ="UPDATE `tbltechnical_assistance` SET 
         `STATUS_REQUEST` = 'Received',
         `START_DATE` = '$date_recieved',
@@ -46,20 +45,55 @@ switch ($option) {
         break;
         echo $query;
     case 'complete':
+        $query = "SELECT * FROM `ta_monitoring` WHERE `STATUS_REQUEST` LIKE '%COMPLETED%' ";
+
+        $result = mysqli_query($conn, $query);
+        $COUNT = '';
+        while($row = mysqli_fetch_array($result))
+        {
+            $COUNT = $row['COUNT']+1;
+        }
+        // ====================================================================
         $insert ="UPDATE `tbltechnical_assistance` SET 
         `STATUS_REQUEST` = 'Completed'
         WHERE `CONTROL_NO` = '$id' ";
         if (mysqli_query($conn, $insert)) {
         } else {
         }
+
+        // =========================================================================
+          $insert1 ="UPDATE `ta_monitoring` SET 
+          `COUNT` = '$COUNT'
+           WHERE `ta_monitoring`.`ID` = 3";
+          if (mysqli_query($conn, $insert1)) {
+          } else {
+          }
             break;
     case 'update_complete':
+        $query = "SELECT * FROM `ta_monitoring` WHERE `STATUS_REQUEST` LIKE '%RATED%' ";
+
+        $result = mysqli_query($conn, $query);
+        $COUNT = '';
+        while($row = mysqli_fetch_array($result))
+        {
+            $COUNT = $row['COUNT']+1;
+        }
+
+
+
         $insert ="UPDATE `tbltechnical_assistance` SET 
         `STATUS_REQUEST` = 'Rated'
         WHERE `CONTROL_NO` = '$id' ";
         if (mysqli_query($conn, $insert)) {
         } else {
         }
+         // =========================================================================
+          $insert1 ="UPDATE `ta_monitoring` SET 
+          `COUNT` = '$COUNT'
+           WHERE `ta_monitoring`.`ID` = 4";
+          if (mysqli_query($conn, $insert1)) {
+          } else {
+          }
     break;
     
     default:
