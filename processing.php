db5523402d8c3b764b3900f7c5bb411329535a3d
charles <charlesodi1324@gmail.com>
2020-06-16 11:07:39 +0800

    A

--- processing.php
+++ processing.php
@@ -273,7 +273,7 @@ function filldataTable()
 
 
             <?php 
-             if($row['STATUS_REQUEST'] != 'Completed')
+             if($row['COMPLETED_DATE'] == '' || )
              {
                  ?>
                 <button    disabled class = "btn btn-danger btn-md col-lg-12 ">
