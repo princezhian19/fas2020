1fd3a31777989b595c97d2f710b0628c749f6788
charlesodi <charlesodi1324@gmail.com>
2020-07-01 12:51:19 +0800

    ASD

--- daily_logs2.php
+++ daily_logs2.php
@@ -1,6 +1,6 @@
 <?php
 $conn=mysqli_connect("localhost","fascalab_2020","w]zYV6X9{*BN","fascalab_2020");
-$username = $_GET['username'];
+$username = $_SESSION['username'];
 $u = mysqli_query($conn,"SELECT emp.FIRST_M,emp.MIDDLE_M,emp.LAST_M,pos.POSITION_M FROM tblemployeeinfo emp LEFT JOIN tbldilgposition pos on pos.POSITION_ID = emp.POSITION_C WHERE emp.UNAME = '$username' ");
 $row = mysqli_fetch_array($u);
 $FIRST_M1 = $row['FIRST_M'];
