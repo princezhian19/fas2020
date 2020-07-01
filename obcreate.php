cbf5d6cfaae42ec62b5ded332266717f97a20b6b
Christian Paul Ferrer <zhianferrer019@gmail.com>
2020-07-01 18:12:38 +0800

    obcreate

--- obcreate.php
+++ obcreate.php
@@ -124,7 +124,7 @@ function app($connect)
                       <tbody id="result">
                       </tbody>
                       </table>
-                      <br>
+                     
                   <!-- Getting PO NUmber -->      
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
             
