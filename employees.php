011071066a232aa91911f00eb848a0cf298b2f54
charlesodi <charlesodi1324@gmail.com>
2020-07-06 11:42:58 +0800

    asd

--- employees.php
+++ employees.php
@@ -64,8 +64,14 @@ if (isset($_POST['submit'])) {
                 <div>
                  <div class="col-xs-1">
                  </div>
+                 <div class="col-xs-2 " style="padding-top: 5px;" >
+                <div >
+                <br>
+              <a href="javascript:void(0);" class="btn btn-success link2 pull-right" data-id="<=$data['id']?>">DTR</a>
+              </div>
+                 </div>
                   <div class="col-xs-2">
-                <div hidden>
+                <div >
                <label>Employement Status </label>
                 <select  class="form-control select2" name="emp_status" id="emp_status">
                   <option selected disabled></option>
@@ -75,15 +81,10 @@ if (isset($_POST['submit'])) {
 
                </div>
                </div>
-               <div class="col-xs-2 " style="padding-top: 5px;" hidden>
-                <div >
-                <br>
-              <a href="javascript:void(0);" class="btn btn-success link2 pull-right" data-id="<=$data['id']?>">DTR</a>
-              </div>
-                 </div>
+               
                </div>
                 
-               <div class="col-xs-2" hidden>
+               <div class="col-xs-2" >
                 <div >
                 <label>Month </label>
                 <select  class="form-control select2" name="month" id="month">
