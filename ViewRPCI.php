d03cd8c3c5b737c10800149e9815f061dd4deb99
charlesodi <charlesodi1324@gmail.com>
2020-07-08 11:50:42 +0800

    asd

--- ViewRPCI.php
+++ ViewRPCI.php
@@ -83,17 +83,46 @@ $username = $_SESSION['username'];
 <script src="dist/js/adminlte.min.js"></script>
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 <script>
-  $(function () {
-    $('').DataTable()
-    $('#example1').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : true,
-      'ordering'    : false,
-      'info'        : false,
-      'autoWidth'   : false
-    })
-  })
+  $(document).ready(function() {
+
+    $('#example1').DataTable( {
+
+              'paging'      : true,
+              'lengthChange': false,
+              'searching'   : true,
+              'ordering'    : false,
+              'info'        : false,
+              'autoWidth'   : false,
+
+              initComplete: function () {
+            this.api().columns(10).every( function () {
+                var column = this;
+                var select = $('<select class="form-control" style="width:100%"><option value=""></option></select>')
+                    .appendTo( $(column.header()) )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+
+        }
+        
+
+    } );
+
+  
+} );
+
+
 </script>
 
 
