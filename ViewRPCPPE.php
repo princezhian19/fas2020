c6663fa95817fb7375c0275ff64f189cb8226503
charlesodi <charlesodi1324@gmail.com>
2020-07-08 11:54:26 +0800

    ljh

--- ViewRPCPPE.php
+++ ViewRPCPPE.php
@@ -84,17 +84,46 @@ $username = $_SESSION['username'];
 
 
 <script>
-  $(function () {
-    $('').DataTable()
-    $('#example1').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : true,
-      'ordering'    : false,
-      'info'        : false,
-      'autoWidth'   : true
-    })
-  })
+  $(document).ready(function() {
+
+    $('#example1').DataTable( {
+
+              'paging'      : true,
+              'lengthChange': false,
+              'searching'   : true,
+              'ordering'    : false,
+              'info'        : false,
+              'autoWidth'   : false,
+
+              initComplete: function () {
+            this.api().columns(2).every( function () {
+                var column = this;
+                var select = $('<select class="form-control" style="width:100%"><option value=""></option></select>')
+                    .appendTo( $(column.header()) )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+
+        }
+        
+
+    } );
+
+  
+} );
+
+
 </script>
 </body>
 </html>
