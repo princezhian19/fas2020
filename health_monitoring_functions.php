e253c1be1200dc8f03175cbb3af07fa897201704
charles <charlesodi1324@gmail.com>
2020-07-09 12:13:23 +0800

    aaa

--- health_monitoring_functions.php
+++ health_monitoring_functions.php
@@ -144,7 +144,7 @@ if(isset($_GET['action'])){
     if($_GET['action'] == 'add')
     {
         add();
-        header('Location:HealthMonitoring.php?username='.$_SESSION['username'].'&division='.$_SESSION['division'].'')
+        header('Location:HealthMonitoring.php?username="'.$_SESSION['username'].'"&division="'.$_SESSION['division'].'"');
     }
     
 }
