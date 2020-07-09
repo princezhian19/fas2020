0b2d611c8e46abf6d5d5cfddcf245cee734cc0c0
charles <charlesodi1324@gmail.com>
2020-07-09 12:59:34 +0800

    ok

--- HealthMonitoring.php
+++ HealthMonitoring.php
@@ -289,7 +289,7 @@ $(document).ready(function()
     username:'<?php echo $username;?>'
   },
   success: function(data) {
- if(data ==1)
+ if(data == 1)
  {
   $('#welcome-modal').modal('hide');
  }else{
