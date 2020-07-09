b8f11135e1930a5524ac38067eea1acacde0bb01
charles <charlesodi1324@gmail.com>
2020-07-09 12:01:13 +0800

    aa

--- HealthMonitoring.php
+++ HealthMonitoring.php
@@ -486,6 +486,24 @@ $(document).ready(function()
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
 <script src="dist/js/adminlte.min.js"></script>
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+
+
+
+
+
+<!-- jQuery 3 -->
+<!-- Bootstrap 3.3.7 -->
+<!-- Select2 -->
+<script src="bower_components/select2/dist/js/select2.full.min.js"></script>
+<!-- InputMask -->
+<script src="plugins/input-mask/jquery.inputmask.js"></script>
+<!-- date-range-picker -->
+<script src="bower_components/moment/min/moment.min.js"></script>
+<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap datepicker -->
+<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- bootstrap color picker -->
+
 <!-- Page script -->
 <script>
 
