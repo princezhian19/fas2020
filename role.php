5db1c64fefea7a4eaacd1407425fe076c35499a9
charlesodi <charlesodi1324@gmail.com>
2020-07-13 16:34:42 +0800

    asd

--- role.php
+++ role.php
@@ -1,64 +1,7 @@
-<script type="text/javascript">
-  function askGoogle()
-{
-    $ch = curl_init();
-
-    curl_setopt(
-        $ch,
-        CURLOPT_URL,
-        "https://www.googleapis.com/geolocation/v1/geolocate?key=[MYAPIKEY]"
-    );
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    $payloadArray = [
-        'considerIp' => false,
-        'wifiAccessPoints' => [
-            [
-                'macAddress'         => '00:25:9c:cf:1c:ac',
-                "signalStrength"     => -25,
-                "signalToNoiseRatio" => -101
-            ],
-            [
-                'macAddress'         => '00:25:9c:cf:1c:ad',
-                "signalStrength"     => -25,
-                "signalToNoiseRatio" => -101
-            ],
-        ],
-    ];
-
-    $payloadJson = json_encode($payloadArray);
-
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        ["Content-type: application/json", "Content-Length: ".strlen($payloadJson)]
-    );
-
-
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadJson);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-
-
-    $result = curl_exec($ch);
-    if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
-    }
-
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    echo "The curl completion is ".$httpCode."<br>";
-
-    $message =  json_decode($result, true); //converts the returned JSON message into an array.
-
-    print_r($message);
-    echo "<br>";
-
-    curl_close($ch);
-}
-
-askGoogle();
-</script>
+<?php 
+echo  $location = file_get_contents('http://freegeoip.net/json/'.$_SERVER['REMOTE_ADDR']);
+echo $location;
+?>
 
  <?php 
  $id = $_GET['id'];
