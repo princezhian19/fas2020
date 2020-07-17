5b7b8784141acdebd89e2bd4b08aed7651873f6c
kevin <kevinvelchez@gmail.com>
2020-07-17 12:25:31 +0800

    automatic computation

--- @disbursementvalue.php
+++ @disbursementvalue.php
@@ -19,7 +19,7 @@ else
  // $results = $conn->prepare("SELECT DISTINCT * FROM app a left join rfq_items rfq on a.id = rfq.app_id where a.id = rfq.app_id ");
  //$results = $conn->prepare("SELECT DISTINCT po.po_date, po.po_no, supplier.supplier_title, pmo.pmo_title,supplier_quote.supplier_id,supplier_quote.rfq_item_id,rfq_items.rfq_id,app.id FROM supplier_quote LEFT JOIN selected_quote ON supplier_quote.id = selected_quote.supplier_quote_id LEFT JOIN supplier ON supplier.id = supplier_quote.supplier_id LEFT JOIN po ON po.id = selected_quote.po_id LEFT JOIN rfq_pmo ON rfq_pmo.rfq_id = selected_quote.rfq_id LEFT JOIN pmo on pmo.id = rfq_pmo.pmo_id LEFT JOIN rfq ON rfq.id = selected_quote.rfq_id LEFT JOIN  rfq_items on rfq_items.rfq_id = selected_quote.rfq_id LEFT JOIN app on app.id = rfq_items.app_id where po_date IS NOT NULL and po_no IS NOT NULL");
 $q = $_POST["query"];
-$results = $conn->prepare("SELECT sum(amount) as amount, payee, particular, datereleased,ors,saronumber,ppa  FROM saroob  WHERE ors LIKE '".$q."%'  ");
+$results = $conn->prepare("SELECT sum(amount) as amount, payee, particular, datereleased,ors,saronumber,ppa  FROM saroob  WHERE ors = '$q'  ");
 
 /* echo "SELECT sum(amount) as amount, payee, particular, datereleased,ors,saronumber,ppa  FROM saroob  WHERE ors LIKE '%".$q."%' ";
 exit(); */
@@ -33,7 +33,7 @@ while($row = $results->fetch(PDO::FETCH_ASSOC))
     $datereleased = $row['datereleased'];
     $dr = date('m/d/Y', strtotime($datereleased));
 
-    $amount = number_format($row['amount'],2);
+    $amount = number_format($row['amount'],2,'.', '');
 
      echo '<tr onclick="javascript:showRow(this);">' .
       
