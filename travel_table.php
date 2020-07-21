d4b4f7a512f56c5c9a65aa9c163cccf337388a4a
charles <charlesodi1324@gmail.com>
2020-07-21 13:06:53 +0800

    FFF

--- travel_table.php
+++ travel_table.php
@@ -124,7 +124,7 @@ echo '<input type = "hidden" id = "hidden_url" value = "'.$link.'"/>';
                   <td class = "label-text">
                     <label>Name: 
                       </td>
-                  <td colspan = 4><input type = "text" class = "form-control" style = "font-weight:bold;"value = "<?php echo viewCompleteName($_GET['username']);?>" /></td>
+                  <td colspan = 4><input type = "text" class = "form-control" style = "font-weight:bold;"value = "<?php echo viewCompleteName($_SESSION['username']);?>" /></td>
                   <td colspan = 2 class = "label-text"><label>Date of Travel: <label style="color: Red;" >*</label> </label></td>
                   <td colspan = 4><input type = "text" class = "form-control datepicker1" id = "datepicker1" value = "<?php echo date('F d, Y');?>"/></td>
                 </tr>
