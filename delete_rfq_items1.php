b8fb1b28dc4866dcb1be731ed34bf872cf1264c0
charlesodi <charlesodi1324@gmail.com>
2020-07-21 14:35:40 +0800

    asd

--- delete_rfq_items1.php
+++ delete_rfq_items1.php
@@ -14,6 +14,16 @@ $purpose = $_GET['purpose'];
 
 $del = mysqli_query($conn,"DELETE FROM pr_items WHERE id = '$id' ");
 
+$selectr = mysqli_query($conn,"SELECT pr_no FROM pr_items WHERE id = '$id'");
+$rowPr = mysqli_fetch_array($selectr);
+$pr_no = $rowPr['pr_no'];
+$app_id = $rowPr['app_id'];
+
+$selectRfqNO = mysqli_query($conn,"SELECT id FROM rfq WHERE pr_no = '$pr_no'");
+$rowP = mysqli_fetch_array($selectRfqNO);
+$rfqid = $rowP['id'];
+$del2 = mysqli_query($conn,"DELETE FROM rfq_items WHERE rfq_id = '$rfqid' AND app_id = '$app_id'");
+
 	 header('location: ViewRFQdetails1.php?id='.$id2.' ');
 	 // header('location: ViewRFQdetails.php?pr_no='.$pr_no.'&pr_date='.$pr_date.'&pmo='.$pmo.'&purpose='.$purpose.' ');
 
