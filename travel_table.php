08696bdec7d15bdb9db544b4a6ba68ec8344d3b7
charles <charlesodi1324@gmail.com>
2020-07-23 16:03:09 +0800

    aaa

--- travel_table.php
+++ travel_table.php
@@ -114,154 +114,28 @@ echo '<input type = "hidden" id = "hidden_url" value = "'.$link.'"/>';
 
 
 <div class="modal fade" id="add_travel_dates">
-        <div class="modal-dialog" style = "width:50%;">
-          <div class="modal-content" >
-            <div class="modal-header">
-              <h4 class="modal-title">Edit Travel Dates</h4>
-                <span type="span" class="close" data-dismiss="modal">&times; </span>
-            </div>
-            <div class="modal-body" style = " max-height: calc(100vh - 200px); overflow-y: auto;">
-              <div class="box-body">
-              <form method = "POST" action = "">
-            <input type = "hidden" name = "hidden_ro" value = "<?php echo $_GET['ro'];?>" />
-
-                <div class="well" style = "padding:10px;">
-
-                  <div class="box-body">
-                    <div class = "row">
-                      <div class = "col-sm-12 col-md-12 col-lg-12">
-                          <div class = "col-sm-6 col-md-6 col-lg-6">
-                          <div class="col-md-12">
-                                <div class="form-group">
-                                  <label>Activity Title</label>
-                                 <?php showActivityTitle();?> 
-                                </div>
-                              </div>
-                              <div class="col-md-12">
-                                <div class="form-group">
-                                  <label>Date</label>
-                                    <div class="input-group">
-                                      <div class="input-group-addon">
-                                        <i class="fa fa-calendar"></i>
-                                    </div>
-                                    <input type="text" name = "date" class="form-control datepicker4" data-inputmask="'alias': 'dd/mm/yyyy'" id = "datepicker4" data-mask required>
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="col-md-6">
-                                <div class="form-group">
-                                  <label>Time Going to Venue</label>
-                                    <input type = "time" name = "from1" class = "form-control "/>
-                                </div>
-                              </div>
-                              <div class="col-md-6">
-                                <div class="form-group">
-                                  <label>&nbsp;</label>
-                                  <input type = "time" name = "to1" class = "form-control"/>
-                                </div>
-                              </div>
-                              <div class="col-md-6">
-                                <div class="form-group">
-                                  <label style = "font-size:13px;">Time Going Home or back to Official Station</label>
-                                    <input type = "time" name = "from2" class = "form-control"/>
-                                </div>
-                              </div>
-                              <div class="col-md-6">
-                                <div class="form-group">
-                                  <label>&nbsp;</label>
-                                  <input type = "time" name = "to2" class = "form-control" style = "margin-top:15px;"/>
-                                </div>
-                              </div>
-                              <div class="col-md-12">
-                                <div class="form-group">
-                                  <label>Others</label>
-                                    <input type="text" name = "others" class="form-control" >
-                                </div>
-                              </div>
-                          </div>
-                          <div class = "col-sm-6 col-md-6 col-lg-6">
-                            <div class="col-md-12 well perdiem">
-                            <div>
-        <!-- <img src="images/50km.png" alt="Flying Kites" style = "width:100%;height:auto;"> -->
-        <div class="form-group">
-                                  <label>
-                                  Per Diem
-                                  </label>
-                                  <label class = "pull-right">
-                                  <input type ="hidden" value = "<?php echo getDistance();?>" id = "distance"/>
-                                  Distance: <?php echo getDistance();?>
-                                  </label>
-                              </div>
-                              <div class="form-group">
-                                <label>
-                                Meals
-                                </label><br>
-                                <input type="checkbox" name="meals"  class="minimal-red checkboxgroup1" id = "cb1"> <b>Will Claim Meals</b><br>
-
-                                <input style = "margin-left:14px" type="checkbox" name="breakfast" class="minimal-red checkboxgroup" id = "breakfast" value = "breakfast"> Breakfast
-                                <input type="checkbox" name="lunch" class="minimal-red checkboxgroup" id= "lunch" value = "lunch"> Lunch
-                                <input type="checkbox" name="dinner"  class="minimal-red checkboxgroup" id="dinner" value = "dinner"> Dinner
-                              </div>
-                              <div class="form-group">
-                                <label>
-                                Accomodation
-                                </label><br>
-                                <input type="checkbox"  name = "accomodation" class="minimal-red" id = "wa" value = "With Accomodation accomodation_chkbox"><b> Will Claim Accomodation</b><br>
-                                <input style = "margin-left:14px" type="checkbox"  name = "with_receipt" class="minimal-red receipt" id = "wr" value ="With Receipt"> With Receipt
-                                <input type="text" disabled name="wor_txt"  id = "wor_txt" class = "borderless" style = "width:50%;"/>
-                                <br>
-                                <input style = "margin-left:14px"type="checkbox"  name = "wor_txt" class="minimal-red receipt" id = "wor" value ="Without Receipt"> Without Receipt
-                              </div>
-        </div>
-                            
-                             
-                            </div>
-                          </div>
-                      </div>
-                    </div>
-                    <div style = "padding:10px;" >
-                    <div class="box-body myTemplate2">
-                      <div class="row ">
-                        <div class="col-md-6">
-                          <div class="form-group">
-                            <label>From</label>
-                              <input type="text" name = "from3[]" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group">
-                            <label>To</label>
-                              <input type = "text" name = "to3[]" class = "form-control"/>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group">
-                            <label>Means of Transportation</label>
-                              
-                              <input type="text" name = "mot[]" class="form-control">
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-group">
-                            <label>Transportation Fare</label>
-                              <input type = "text" name = "transpo_fare[]" class = "form-control"  />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                    <span class = "btn btn-success btn-md pull-right" id= "add_fare">Add Fare</span>
-                  </div>
-
-
-
-                  </div>
+  <div class="modal-dialog" style = "width:50%;">
+    <div class="modal-content" >
+      <div class="modal-header">
+        <h4 class="modal-title">Edit Travel Dates</h4>
+        <span type="span" class="close" data-dismiss="modal">&times; </span>
+      </div>
+        <div class="modal-body" style = " max-height: calc(100vh - 200px); overflow-y: auto;">
+          <div class="box-body">
+            <form method = "POST" action = "">
+              <input type = "hidden" name = "hidden_ro" value = "<?php echo $_GET['ro'];?>" />
+               <div id = "travelDate_panel">
+               </div>
+                
 
             
-
+<!-- 
                   <div class = "well" style = "padding:10px;" id = "travelPanel">
-                  </div>
-                  <button type = "submit" class = "btn btn-success btn-md pull-right">Save </button>
+                  </div> -->
+
+                  <button type = "submit" class = "btn btn-success btn-md pull-right">Save Changes</button>
+                  <button class = "btn btn-primary btn-md pull-right" id= "add_fare" style = "margin-right:10px">Add Fare</button>
+
                   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
               </form>
@@ -379,50 +253,49 @@ echo '<input type = "hidden" id = "hidden_url" value = "'.$link.'"/>';
                 $('#or').val(data[2]);
                 $.ajax({
                   type: 'POST',
-                  url: 'testtime.php',
+                  url: 'editTravelData.php',
                   data: (
                     {
                       ro:RO,
                       uname:username,
-                      action:"edit",
                     }),
                   cache: false,
                   success: function(data)
                   {
-                    $('#tbl2').html(data);
+                    $('#travelDate_panel').html(data);
 
                   }
                 });
               
-                $.ajax({
-                  type: 'POST',
-                  url: 'getTable1.php',
-                  data: ({
-                    "username":username,
-                    "purpose":RO
-                    }),
-                  cache: false,
-                  success: function(data2)
-                  {
-                    $('#tbl1').html(data2);
+                // $.ajax({
+                //   type: 'POST',
+                //   url: 'getTable1.php',
+                //   data: ({
+                //     "username":username,
+                //     "purpose":RO
+                //     }),
+                //   cache: false,
+                //   success: function(data2)
+                //   {
+                //     $('#tbl1').html(data2);
 
-                  }
-                });
-                $.ajax({
-                  type: 'POST',
-                  url: 'getTable3.php',
-                  data: ({
-                    "username":username,
-                    "purpose":RO,
-                    "action":"edit"
-                    }),
-                  cache: false,
-                  success: function(data3)
-                  {
-                    $('#tbl3').html(data3);
+                //   }
+                // });
+                // $.ajax({
+                //   type: 'POST',
+                //   url: 'getTable3.php',
+                //   data: ({
+                //     "username":username,
+                //     "purpose":RO,
+                //     "action":"edit"
+                //     }),
+                //   cache: false,
+                //   success: function(data3)
+                //   {
+                //     $('#tbl3').html(data3);
 
-                  }
-                });
+                //   }
+                // });
               });
 
 
