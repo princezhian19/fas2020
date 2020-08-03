ad9fe7447dda011160f1c51082bae1b930e2cb22
kevin <kevinvelchez@gmail.com>
2020-08-03 10:11:42 +0800

    date

--- @disbursementtable.php
+++ @disbursementtable.php
@@ -183,7 +183,7 @@ $timeNow = (new DateTime('now'))->format('m/d/Y');
                                   <td><?php echo $datereleased;?></td>
                                 <?php else: ?>
 
-                                <!-- <td><a class="btn btn-success btn-xs" href='release_dv.php?id=<?php echo $id; ?>' >Release</a> </td> -->
+                                <td><!-- <a class="btn btn-success btn-xs" href='release_dv.php?id=<?php echo $id; ?>' >Release</a>  --></td>
                                <?php endif ?>
 
                               <?php endif ?>
