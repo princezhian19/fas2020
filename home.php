5bac5f6f4f6ec6b2900647f5ea3a4e7891d813a3
charles <charlesodi1324@gmail.com>
2020-10-02 09:56:05 +0800

    a

--- home.php
+++ home.php
@@ -67,18 +67,18 @@ only screen and (max-width: 760px),
 (min-device-width: 768px) and (max-device-width: 1024px)  {
 
 	/* Force table to not be like tables anymore */
-	#tbl { 
+	table, thead, tbody, th, td, tr { 
 		display: block; 
 	}
 	
 	/* Hide table headers (but not display: none;, for accessibility) */
-	#tbl thead tr { 
+	thead tr { 
 		position: absolute;
 		top: -9999px;
 		left: -9999px;
 	}
 	
-	#tbl tr { border: 1px solid #ccc; }
+	tr { border: 1px solid #ccc; }
 	
 
 	
