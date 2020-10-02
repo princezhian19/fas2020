ba6f11d39aca816b7cf0c0c23043bbd4c39671c6
charles <charlesodi1324@gmail.com>
2020-10-02 09:44:20 +0800

    a

--- home.php
+++ home.php
@@ -62,33 +62,7 @@ Max width before this PARTICULAR table gets nasty
 This query will take effect for any screen smaller than 760px
 and also iPads specifically.
 */
-@media 
-only screen and (max-width: 760px),
-(min-device-width: 768px) and (max-device-width: 1024px)  {
 
-	/* Force table to not be like tables anymore */
-	table, thead, tbody, th, td, tr { 
-		display: block; 
-	}
-	
-	/* Hide table headers (but not display: none;, for accessibility) */
-	thead tr { 
-		position: absolute;
-		top: -9999px;
-		left: -9999px;
-	}
-	
-	tr { border: 1px solid #ccc; }
-	
-
-	
-
-	
-	/*
-	Label the data
-	*/
-
-}
   </style>
 </head>
 <?php include 'home_modal.php';?>
